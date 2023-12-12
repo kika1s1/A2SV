412a88de142027280459af92ad6c9cdcb82e60e1
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-12 21:23:32 +0300

    Time: 60 ms (75.46%) | Memory: 18.3 MB (26.23%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        dictRep = Counter(nums)
+        for i, value in dictRep.items():
+            if value == 1:
+                return i
+        return 0
