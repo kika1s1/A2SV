df8847842552c457488701cce5f6ce585d5fbd7e
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-12 11:38:07 +0300

    Time: 123 ms (32.90%) | Memory: 17.2 MB (46.91%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.py
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.py
@@ -1,14 +1,13 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
+     
         ans = []
         counter = 1
         for i in range(len(s)):
             if s[i] not in ans:
                 ans.append(s[i])
             else:
-                
+
                 counter += 1
                 ans.clear()
                 ans.append(s[i])
