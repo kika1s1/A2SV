5338b7c9548f2853a650639affe892b4f2f8b336
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-19 21:44:27 +0300

    Time: 54 ms (93.41%) | Memory: 16.3 MB (56.68%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.py
@@ -1,9 +1,9 @@
 class Solution:
     def findLucky(self, arr: List[int]) -> int:
-        arr.sort()
+        
         ans = -1
         repHash = Counter(arr)
         for i, j in repHash.items():
             if i == j:
-                ans = i
+                ans = max(ans, i)
         return ans
