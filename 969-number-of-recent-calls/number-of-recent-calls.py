a56c072041816a6c5e1c9c6eb3ba65406850e069
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-21 09:38:51 +0300

    Time: 187 ms (100.00%) | Memory: 23 MB (5.06%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -0,0 +1,8 @@
+class RecentCounter:
+    def __init__(self):
+        self.queue = deque() 
+    def ping(self, t: int) -> int:
+        self.queue.append(t) 
+        while self.queue[0] < t - 3000: 
+            self.queue.popleft() 
+        return len(self.queue)                           
