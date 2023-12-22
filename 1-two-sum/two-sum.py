113e57cc4a954a4894860e81254376474a7c6348
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-22 22:39:28 +0300

    Time: 78 ms (42.33%) | Memory: 19 MB (6.48%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,18 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, j in enumerate(nums):
-            check = target - j
-            if check in nums and nums.index(check) != i:
-                return [i, nums.index(check)]
+      def binary_search(self, xs, x, lo=0, hi=None):
+        while True:
+          if lo >= hi:
+              return None
+          mid = (lo + hi) // 2
+          if xs[mid][0] == x:
+              return mid
+          elif xs[mid][0] > x:
+              hi = mid
+          else:
+              lo = mid+1
+                
+      def twoSum(self, nums, target):
+        sort = sorted((n,i) for i,n in enumerate(nums))
+        for i in range(len(sort)):
+          if j := self.binary_search(sort, target-sort[i][0], i, len(sort)):
+             return [sort[i][1], sort[j][1]]
