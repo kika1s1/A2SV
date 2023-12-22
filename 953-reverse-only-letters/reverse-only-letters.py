b72a9b849bec81596278c97c33dc0b1b3f8ff059
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-22 18:41:38 +0300

    Time: 35 ms (76.67%) | Memory: 17.3 MB (20.06%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.py
+++ 953-reverse-only-letters/reverse-only-letters.py
@@ -1,15 +1,16 @@
 class Solution:
     def reverseOnlyLetters(self, s: str) -> str:
-        ans = ''
-        curr = ''
-        for i in range(len(s)):
-            if s[i].isalpha() == True:
-                curr += s[i]
-        ind = -1
-        for i in range(len(s)):
-            if s[i].isalpha() == True:
-                ans += curr[ind]
-                ind -= 1
-            else:
-                ans += s[i]
-        return ans
+        if len(s) == 1:
+            return s
+        s = list(s)
+        l, r = 0, len(s)-1
+        while l <=r:
+            if s[l].isalpha() and s[r].isalpha():
+                s[l], s[r] = s[r], s[l]
+                r -=1
+                l +=1
+            if not s[l].isalpha():
+                l +=1
+            if not s[r].isalpha():
+                r -=1
+        return "".join(s)
