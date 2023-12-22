3366d2502318f3e6a5008861705989c66d4c3b33
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-22 11:36:17 +0300

    Time: 641 ms (9.58%) | Memory: 17.7 MB (6.01%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,9 +1,17 @@
+def swap(a,b):
+    return b,a
+def backtrack(a, ans, l, r):
+    if l==r:
+        if a not in ans:
+            ans.append(list(a))
+        return
+    for i in range(l,r):
+        a[i], a[l] = swap(a[i], a[l])
+        backtrack(a, ans, l+1, r)
+        a[i], a[l] = swap(a[i], a[l])
+    return
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        allList = list(permutations(nums))
         ans = []
-        for i in allList:
-            if list(i) not in ans:
-                ans.append(list(i))
-        return ans
-                
+        backtrack(nums, ans, 0, len(nums))
+        return ans 
