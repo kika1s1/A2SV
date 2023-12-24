0e3f9d8b5ba6a51940d385b3bc74cb5c35fa6547
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-24 21:30:00 +0300

    Time: 39 ms (57.10%) | Memory: 17.2 MB (5.15%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+        
+        ans = [0] * len(nums)
+        for i in range(len(nums)):
+            if i-1 > 0:
+                ans[i] = nums[i] + max(ans[:i-1])
+            else:
+                ans[i] = nums[i]
+        
+        return max(ans)
