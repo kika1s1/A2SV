7c970c1056e58c122deb94b58d0a2f4be3b429a1
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-26 01:31:49 +0300

    Time: 378 ms (8.55%) | Memory: 17 MB (7.40%) - LeetSync

--- 507-perfect-number/perfect-number.py
+++ 507-perfect-number/perfect-number.py
@@ -1,12 +1,12 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-            if num == 1:
-                return False
-            
-            sum = 1
-            for i in range(2, int(num ** 0.5) + 1):
-                if num % i == 0:
-                    sum += i + num //i
-        
-            return sum == num
-        
+        if num % 2 == 1:
+            return False
+        count = 1
+        for i in range(2, num // 2 + 1):
+            if num % i == 0:
+                right = num // i
+                if right <= i:
+                    break
+                count += (i + right)
+        return count == num
