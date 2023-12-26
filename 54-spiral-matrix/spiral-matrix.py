79725e3fb75728c97e1e9343c824371f8a713a66
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-26 16:21:34 +0300

    Time: 39 ms (50.12%) | Memory: 17.2 MB (6.87%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,32 +1,26 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix:
-            return []
-        
-        rows, cols = len(matrix), len(matrix[0])
-        top, bottom = 0, rows - 1
-        left, right = 0, cols - 1
-        direction = 0
-        result = []
-        
-        while top <= bottom and left <= right:
-            if direction == 0:  # Traverse from left to right
-                for i in range(left, right + 1):
-                    result.append(matrix[top][i])
-                top += 1
-            elif direction == 1:  # Traverse from top to bottom
-                for i in range(top, bottom + 1):
-                    result.append(matrix[i][right])
-                right -= 1
-            elif direction == 2:  # Traverse from right to left
-                for i in range(right, left - 1, -1):
-                    result.append(matrix[bottom][i])
-                bottom -= 1
-            elif direction == 3:  # Traverse from bottom to top
-                for i in range(bottom, top - 1, -1):
-                    result.append(matrix[i][left])
-                left += 1
-            
-            direction = (direction + 1) % 4
-        
-        return result
+        xmin = -1
+        xmax = len(matrix)
+        ymin = -1
+        ymax = len(matrix[0])
+        x = y = 0
+        res = [matrix[0][0]]
+
+        while xmin <= xmax and ymin <= ymax:
+            for i, direction in enumerate([[0,1],[1,0],[0,-1],[-1,0]]):
+                while xmin < x + direction[0] < xmax and ymin < y + direction[1] < ymax:
+                    x += direction[0]
+                    y += direction[1]
+                    res.append(matrix[x][y])
+                if i == 0:
+                    xmin += 1
+                elif i == 1:
+                    ymax -= 1
+                elif i == 2:
+                    xmax -= 1
+                elif i == 3:
+                    ymin += 1
+                if xmin + 1 == xmax or ymin + 1 == ymax:
+                    break
+        return res
