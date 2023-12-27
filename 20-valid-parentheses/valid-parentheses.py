1fcd5408959a2e8ed8bf5d417703bc8fd2e13b20
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-27 23:03:54 +0300

    Time: 18 ms (34.39%) | Memory: 13.4 MB (75.39%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,21 +1,32 @@
 class Solution(object):
     def isValid(self, s):
-        pair = {
-            "(": ")",
-            "{": "}",
-            "[": "]"
-        }
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        matches = {")": "(", "]": "[", "}": "{"}
+
+        if len(s) < 2 or s[0] in matches:
+            return False
+
         stack = []
-        for i in s:
-            check = pair.get(i, 0)
-            if check == 0 and len(stack) == 0:
+
+        for bracket in s:
+            if bracket in "([{":
+                stack.append(bracket)
+                continue
+
+            if len(stack) <= 0:
                 return False
-            if i in pair:
-                stack.append(i)
-            elif stack:
-                if i == pair[stack[-1]]:
-                    stack.pop()
-                else:
-                    return False
-        print(stack)
-        return len(stack) == 0
+
+            if stack[-1] == matches[bracket]:
+                stack.pop()
+            else:
+                return False
+
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+        
