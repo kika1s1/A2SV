ff6d0616f1090343bc921f80906148f02b609444
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-29 00:51:05 +0300

    Time: 149 ms (60.43%) | Memory: 18.3 MB (5.56%) - LeetSync

---
+++ 838-design-linked-list/design-linked-list.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class MyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def get(self, index: int) -> int:
+        if index < 0 or index >= self.length:
+            return -1
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp.value
+
+    def addAtHead(self, val: int) -> None:
+        new_node = Node(val)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def addAtTail(self, val: int) -> None:
+        new_node = Node(val)
+        if self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.length:
+            return
+        if index == 0:
+            self.addAtHead(val)
+        elif index == self.length:
+            self.addAtTail(val)
+        else:
+            new_node = Node(val)
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            new_node.next = temp.next
+            temp.next = new_node
+            self.length += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.length:
+            return
+        if index == 0:
+            self.head = self.head.next
+            if self.head is None:
+                self.tail = None
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            temp.next = temp.next.next
+            if temp.next is None:
+                self.tail = temp
+        self.length -= 1
