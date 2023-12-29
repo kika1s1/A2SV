a9c988e8661ceb644c3dfc6a87069c10173ef244
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-30 00:23:24 +0300

    Time: 72 ms (6.70%) | Memory: 17.3 MB (10.71%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -5,18 +5,34 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        ans = ""
-        n, m = len(word1), len(word2)
-        i, j = 0, 0
-        while i < n and j < m:
-            ans +=word1[i]
-            ans +=word2[j]
-            i +=1
-            j +=1
-        while i < n:
-            ans +=word1[i]
-            i +=1
-        while j < m:
-            ans +=word2[j]
-            j +=1
-        return ans
+        # ans = ""
+        # n, m = len(word1), len(word2)
+        # i, j = 0, 0
+        # while i < n and j < m:
+        #     ans +=word1[i]
+        #     ans +=word2[j]
+        #     i +=1
+        #     j +=1
+        # while i < n:
+        #     ans +=word1[i]
+        #     i +=1
+        # while j < m:
+        #     ans +=word2[j]
+        #     j +=1
+        # return ans
+
+        
+        result = ""
+        n1 = len(word1)
+        n2 = len(word2)
+        idx1, idx2 = 0, 0
+        while idx1 < n1 or idx2 < n2:
+            if idx1 < n1:
+                result += word1[idx1]
+                idx1 += 1
+
+            if idx2 < n2:
+                result += word2[idx2]
+                idx2 += 1
+
+        return result  
