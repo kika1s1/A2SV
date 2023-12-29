5224f7658840aa3edcb222144f670bf2c83a1064
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-30 00:21:43 +0300

    Time: 61 ms (6.70%) | Memory: 17.3 MB (10.71%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -5,7 +5,18 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        listWord1 = list(word1)
-        for i in range(len(word2)):
-            listWord1.insert(2*i+1, word2[i])
-        return "".join(listWord1)
+        ans = ""
+        n, m = len(word1), len(word2)
+        i, j = 0, 0
+        while i < n and j < m:
+            ans +=word1[i]
+            ans +=word2[j]
+            i +=1
+            j +=1
+        while i < n:
+            ans +=word1[i]
+            i +=1
+        while j < m:
+            ans +=word2[j]
+            j +=1
+        return ans
