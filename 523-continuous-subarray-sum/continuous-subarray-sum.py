35bdf44a406dbe34fc977047b7604b4c09dcdb71
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-30 19:58:15 +0300

    Time: 880 ms (91.44%) | Memory: 37.1 MB (11.18%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        # for i in range(len(nums)):
+        #     for j in range(i+2, len(nums)+1):
+        #         sub = nums[i:j]
+        #         if sum(sub) % k == 0:
+        #             return True
+        # return False
+
+        reminder = {0:-1}
+        total = 0
+        for i, n in enumerate(nums):
+            total +=n
+            r = total%k
+            if r not in reminder:
+                reminder[r] = i
+            elif i - reminder[r] >1:
+                return True
+        return False
