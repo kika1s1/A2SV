0a248d121362d9110c7cef2bf385f41276ae0618
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-30 11:26:32 +0300

    Time: 59 ms (5.34%) | Memory: 17.3 MB (6.90%) - LeetSync

--- 476-number-complement/number-complement.py
+++ 476-number-complement/number-complement.py
@@ -1,13 +1,5 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        ans = bin(num)[2:]
-        b = ""
-        for i in ans:
-            if i == "0":
-                b +="1"
-            else:
-                b +="0"
-        decimal = 0
-        for digit in b:
-            decimal = decimal * 2 + int(digit)
-        return decimal
+        num_bits = num.bit_length()
+        bitmask = (1 << num_bits) - 1
+        return num ^ bitmask
