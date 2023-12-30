6d08bcf40aeb989fd71f63704867fa09771dcb8d
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-30 23:06:40 +0300

    Time: 352 ms (5.13%) | Memory: 18.7 MB (5.98%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkPossibility(self, nums: List[int]) -> bool:
+        streak1 = 0
+        streak2 = 0
+        n = nums.copy()
+        if len(nums) == 1:
+            return True
+        for i in range(1, len(nums)):
+            if nums[i-1] > nums[i]:
+                nums[i-1] = nums[i]
+                break
+        for i in range(1, len(n)):
+            if n[i-1] > n[i]:
+                n[i] = n[i-1]
+                break
+
+        for i in range(1, len(n)):
+            if n[i-1] > n[i]:
+                streak1 += 1
+        for i in range(1, len(nums)):
+            if nums[i-1] > nums[i]:
+                streak2 += 1
+        if streak1 == 0 or streak2 == 0:
+            return True
+
+        return False
+
