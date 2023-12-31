222055dfbcdb23a67c7bb99674d4c7ee2714f884
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-31 16:30:15 +0300

    Time: 141 ms (26.73%) | Memory: 17.6 MB (24.27%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -2,14 +2,13 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         if s == s[::-1]:
             return True
-        else:
-            i = 0
-            j = len(s) - 1
+        i = 0
+        j = len(s) - 1
 
-            while i < j:
-                if s[i] != s[j]:
-                    s1 = s[0:i] + s[i+1:]
-                    s2 = s[0:j] + s[j+1:]
-                    return s1 == s1[::-1] or s2 == s2[::-1]
-                i += 1
-                j -= 1
+        while i < j:
+            if s[i] != s[j]:
+                s1 = s[0:i] + s[i+1:]
+                s2 = s[0:j] + s[j+1:]
+                return s1 == s1[::-1] or s2 == s2[::-1]
+            i += 1
+            j -= 1
