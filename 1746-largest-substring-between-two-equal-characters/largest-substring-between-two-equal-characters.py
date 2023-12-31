eea47518a466334e5d2ef8fcf5e2b3fab6cfd8db
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-31 08:37:16 +0300

    Time: 76 ms (6.83%) | Memory: 17.3 MB (5.61%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.py
@@ -1,20 +1,10 @@
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
-        # maximum = -1
-        # for i in s:
-        #     r = s.rindex(i)
-        #     l = s.index(i)+1
-        #     maximum = max(maximum, r-l)
+        maximum = -1
+        for i in s:
+            r = s.rindex(i)
+            l = s.index(i)+1
+            maximum = max(maximum, r-l)
        
-        # return maximum
-        length = -1
-        myhash= {}
-        
-        for i in range(len(s)):
-            
-            if s[i] not in myhash:
-                myhash[s[i]] = i
-            else:
-                length = max(length , i- myhash.get(s[i]) -1)
-                
-        return length
+        return maximum
+       
