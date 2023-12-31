4f1e936461abe808b87355b73c0023cf2f38fe14
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-31 18:08:49 +0300

    Time: 85 ms (5.41%) | Memory: 17.5 MB (5.20%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,9 +1,13 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        new_path = []
-        for folder in path.split('/'):
-            if folder!='' and folder!='.' and folder!='..':
-                new_path.append(folder) 
-            elif folder=='..' and len(new_path)>0:
-                    new_path.pop()   
-        return '/'+ ('/').join(new_path)
+        components = path.split('/')
+        stack = []
+
+        for componenet in components:
+            if componenet == '..':
+                if stack:
+                    stack.pop()
+            elif componenet and componenet != '.':
+                stack.append(componenet)
+        return '/' + '/'.join(stack)
+        
