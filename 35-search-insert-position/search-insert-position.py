db3fc5df12a93bd11191c25090e00ae085d809f6
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-31 15:54:00 +0300

    Time: 33 ms (30.37%) | Memory: 14.1 MB (42.75%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def searchInsert(self, nums, target):
-        #717822i141-Ragul A.S
         left,right=0,len(nums)
         while left<right:
             mid=left+(right-left)//2
