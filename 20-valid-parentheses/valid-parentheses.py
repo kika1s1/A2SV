acf2ef5346c772300398a0ecd6f38a21b0d8a906
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2023-12-31 10:53:45 +0300

    Time: 25 ms (7.59%) | Memory: 13.5 MB (46.74%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,29 +4,43 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-
-        matches = {")": "(", "]": "[", "}": "{"}
-
-        if len(s) < 2 or s[0] in matches:
-            return False
-
         stack = []
-
-        for bracket in s:
-            if bracket in "([{":
-                stack.append(bracket)
-                continue
-
-            if len(stack) <= 0:
+        matches = {")": "(", "]": "[", "}": "{"}
+        for i in s:
+            if len(stack) == 0 and i in matches:
                 return False
-
-            if stack[-1] == matches[bracket]:
-                stack.pop()
+            elif i not in matches:
+                stack.append(i)
             else:
-                return False
+                if stack[-1] == matches[i]:
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0
+            
+            
 
-        if len(stack) == 0:
-            return True
-        else:
-            return False
+
+        # if len(s) < 2 or s[0] in matches:
+        #     return False
+
+        # stack = []
+
+        # for bracket in s:
+        #     if bracket in "([{":
+        #         stack.append(bracket)
+        #         continue
+
+        #     if len(stack) <= 0:
+        #         return False
+
+        #     if stack[-1] == matches[bracket]:
+        #         stack.pop()
+        #     else:
+        #         return False
+
+        # if len(stack) == 0:
+        #     return True
+        # else:
+        #     return False
         
