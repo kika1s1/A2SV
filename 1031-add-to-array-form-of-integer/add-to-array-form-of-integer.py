1b7d70dc15d6c4eba0edd269970b7c2693b92589
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-02 16:47:43 +0300

    Time: 248 ms (50.77%) | Memory: 18.2 MB (18.59%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.py
@@ -1,13 +1,10 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        n = 0
-        for ele in num:
-            n = (n*10) + ele
-        
-        n = n+k
-        
-        num = []
-        while n > 0:
-            num.insert(0, n % 10)  
-            n //= 10 
+        for i in range(len(num) - 1, -1, -1):
+            if not k: break            
+            k, num[i] = divmod(num[i] + k, 10)
+        while k:
+            k, a = divmod(k, 10)
+            num = [a] + num                        
         return num
+            
