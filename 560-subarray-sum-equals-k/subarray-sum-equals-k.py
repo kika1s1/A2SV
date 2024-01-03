e8f2d89c49dc20beba32b4560925e8562d2c036b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-03 10:25:08 +0300

    Time: 449 ms (5.01%) | Memory: 20.3 MB (30.78%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,20 +1,12 @@
-class Solution(object):
-    def subarraySum(self, nums, k):
-        
-        currSum = 0
-        ansCount = 0
-        prevSums = {0:1}        
-        
-        for num in nums:
-            
-            currSum += num
-            
-            if currSum - k in prevSums:
-                ansCount += prevSums[currSum - k]
-                
-            if currSum in prevSums:
-                prevSums[currSum] += 1
-            else:
-                prevSums[currSum] = 1
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        res = 0
+        temp = 0
+        d = {0:1}
 
-        return ansCount
+        for n in nums:
+            temp += n
+            if temp - k in d:
+                res += d[temp-k]
+            d[temp] = d.get(temp, 0) + 1
+        return res
