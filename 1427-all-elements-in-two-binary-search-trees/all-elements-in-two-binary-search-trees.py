7b75227ba9a63fbe7eff778951099b5ccbec142f
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-03 09:37:37 +0300

    Time: 325 ms (18.09%) | Memory: 21.2 MB (75.60%) - LeetSync

---
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        ans = []
+
+        def dfs(node):
+            if node:
+                ans.append(node.val)
+                dfs(node.left)
+                dfs(node.right)
+
+        dfs(root1)
+        dfs(root2)
+
+        return sorted(ans)
