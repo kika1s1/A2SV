57feee3aec62921b6f899ce1a6c7f4b93d0bcb3c
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-09 09:55:40 +0300

    Time: 38 ms (67.72%) | Memory: 17.5 MB (10.09%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,25 +1,20 @@
-class Solution(object):
-    def reverse(self, x):
-        INT_MAX = 2**31 - 1
-        INT_MIN = -2**31
+class Solution:
+    def reverse(self, x: int) -> int: 
+        MIN, MAX = -2**31, 2**31-1
+        ans = 0
+        b = abs(x)
+        l = len(str(b))
+        times = -1 if x < 0 else 1
+        x = abs(x)
+        pw = 0
+        while x >= 1:
+            rem = x % 10
+            pw += 1
+            ans += rem * (10 ** (l-pw))
 
-        if x == 0:
+            x //= 10
+        if ans > MAX:
             return 0
-        
-        if INT_MIN <= x <= INT_MAX:
-            def remove_leading_zeros(n):
-                while n and n[0] == '0':
-                    n = n[1:]  # Remove leading zero by slicing
-                return n
-
-            if x < 0:
-                reversed_x = int(str(x)[1:][::-1])  # Reversing the positive part
-                reversed_x = -reversed_x  # Make negative after reversing
-            else:
-                reversed_x = int(str(x)[::-1])
-
-            reversed_x = remove_leading_zeros(str(reversed_x))
-
-            return int(reversed_x) if INT_MIN <= int(reversed_x) <= INT_MAX else 0
-        else:
+        if x < MIN:
             return 0
+        return ans*times
