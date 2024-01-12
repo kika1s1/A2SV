0d464a84d62263528adfd9c8f7674ae6ded1d385
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-12 14:40:22 +0300

    Time: 38 ms (59.51%) | Memory: 17.3 MB (11.84%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.py
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -1,3 +1,9 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        return sqrt(num) == int(sqrt(num))
+        # return sqrt(num) == int(sqrt(num))
+        # for i in range(num+1):
+        #     if i*i == num:
+        #         return True
+        # return False
+        s=int(num**0.5)
+        return s*s==num
