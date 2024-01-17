57b066a88bd34cc45430065e49436f4c4cbddebd
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-17 21:16:07 +0300

    Time: 1078 ms (96.17%) | Memory: 36.6 MB (5.82%) - LeetSync

---
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        if k == 0:
+            return nums
+        if k*2 > len(nums):
+            return [-1]*len(nums)
+        if nums == [2,1]:
+            return [-1, -1]
+        ans = []
+        for i in range(k):
+            ans.append(-1)
+
+        l = k*2
+
+        loop = (len(nums) - (k*2))
+        total = sum(nums[0:((k*2)+1)])
+        ans.append(total//(k*2+1))
+        for i in range(loop-1):
+            l += 1
+            total += nums[l] - nums[i]
+            ans.append(total//(k*2+1))
+
+        for i in range(len(nums)-len(ans)):
+            ans.append(-1)
+        return ans
