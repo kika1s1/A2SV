09768d49b7a4e4ce484fb63ef6144e185df88116
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-18 09:07:58 +0300

    Time: 28 ms (95.13%) | Memory: 17.4 MB (15.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,8 +1,17 @@
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         one, two = 1, 1
+#         for i in range(n-1):
+#             temp = one
+#             one = one + two
+#             two = temp
+#         return one 
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-        for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
-        return one 
+        @cache
+        def f(n):
+            if n<=2: return n
+            return f(n-1)+f(n-2)
+        return f(n)
+        
