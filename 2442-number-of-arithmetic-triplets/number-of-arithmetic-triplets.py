9457f3e8d2ed9f20e6df6a95a20ef8fada7c0ac9
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-22 22:05:50 +0300

    Time: 35 ms (95.91%) | Memory: 16.5 MB (55.70%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -1,10 +1,3 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        
-        ans = 0
-        n = len(nums)
-        for i in range(n):
-            if nums[i] + diff in nums and nums[i] + 2 * diff in nums:
-                ans += 1
-        
-        return ans
+        return sum([1 for i in nums if i-diff in nums and i+diff in nums])
