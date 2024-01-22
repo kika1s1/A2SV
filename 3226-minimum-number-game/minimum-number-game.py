e105d4aa6dbc71452772ead8729b997065aac7fc
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-22 23:31:15 +0300

    Time: 50 ms (76.45%) | Memory: 16.6 MB (99.15%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.py
+++ 3226-minimum-number-game/minimum-number-game.py
@@ -1,14 +1,6 @@
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        arr = []
-        cnt = 0
-        
-        while len(nums) !=0:
-            temp = min(nums)
-            nums.remove(temp)
-            sTemp = min(nums)
-            nums.remove(sTemp)
-            arr.append(sTemp)
-            arr.append(temp)
-        return arr
-
+        nums.sort()
+        for i in range(0, len(nums)-1, 2):
+            nums[i], nums[i+1] = nums[i+1], nums[i]
+        return nums
