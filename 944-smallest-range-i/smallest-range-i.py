b672b1530f868ddb46919d3b6633cc83a5808c75
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-24 09:00:47 +0300

    Time: 98 ms (79.94%) | Memory: 17.8 MB (58.07%) - LeetSync

--- 944-smallest-range-i/smallest-range-i.py
+++ 944-smallest-range-i/smallest-range-i.py
@@ -1,6 +1,12 @@
+# class Solution:
+#     def smallestRangeI(self, nums: List[int], k: int) -> int:
+#         mx = max(nums)
+#         mn = min(nums)+k
+#         ans = (mx - mn - k) if mx - mn >= k else 0
+#         return ans
 class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
-        mx = max(nums)
-        mn = min(nums)+k
-        ans = (mx - mn - k) if mx - mn >= k else 0
-        return ans
+        left = min(nums) + k
+        right = max(max(nums) - k, left)
+
+        return right - left
