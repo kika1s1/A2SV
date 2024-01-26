35d6176fbae9955939f178919a4f1f9e5a79c64a
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-26 20:26:07 +0300

    Time: 427 ms (94.57%) | Memory: 19.6 MB (72.79%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -16,21 +16,37 @@
 
 #         return maxim
 
+# class Solution:
+#     def longestSubarray(self, nums: List[int]) -> int:
+#         prev=0
+#         count=0
+#         ls=[]
+#         visit=False
+#         for i in nums:
+#             if i==0:
+#                 visit=True
+#                 ls.append(prev+count)
+#                 prev=count
+#                 count=0
+#             if i==1:
+#                 count+=1
+#         if not visit:
+#             count-=1
+#         ls.append(count+prev)
+#         return max(ls)
+
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        prev=0
-        count=0
-        ls=[]
-        visit=False
-        for i in nums:
-            if i==0:
-                visit=True
-                ls.append(prev+count)
-                prev=count
-                count=0
-            if i==1:
-                count+=1
-        if not visit:
-            count-=1
-        ls.append(count+prev)
-        return max(ls)
+        r=l=0
+        k=1
+
+        for r in range(len(nums)):
+            if nums[r]==0:
+                k -=1
+            if k<0:
+                if nums[l] == 0:
+                    k+=1
+                
+                l+=1
+        
+        return r-l
