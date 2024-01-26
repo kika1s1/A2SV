c843e6dcf592b0a7386190cc9d2020e3e65057c2
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-26 18:43:03 +0300

    Time: 176 ms (57.63%) | Memory: 25 MB (26.73%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,17 +1,19 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        res = [1]*len(nums)
-        prefix = 1
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        leftProduct = [nums[0]]
+        for i in range(1, len(nums)):
+            leftProduct.append(leftProduct[i-1]*nums[i])
+        nums.reverse()
+        rightProduct = [nums[0]]
+        for i in range(1, len(nums)):
+            rightProduct.append(rightProduct[i-1]*nums[i])
+        rightProduct.reverse()
+        ans = []
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums)-1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
-            
+            if i == 0:
+                ans.append(rightProduct[i+1])
+            elif i == len(nums)-1:
+                ans.append(leftProduct[i-1])
+            else:
+                ans.append(leftProduct[i-1] * rightProduct[i+1])
+        return ans
