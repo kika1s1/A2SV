5aa7f7f48a206f7bf381bcc74df6b9f11e9f1dcf
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-27 18:38:47 +0300

    Time: 32 ms (86.73%) | Memory: 16.6 MB (69.40%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,5 +1,6 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
+        #tamirat
         minam = float("inf")
         for i in range(len(blocks)-(k-1)):
             sub = blocks[i:k+i]
