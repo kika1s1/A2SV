c50ae232c626b4e29f0b6e23d06adec349f72969
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-27 18:26:08 +0300

    Time: 27 ms (96.63%) | Memory: 16.6 MB (71.79%) - LeetSync

---
+++ 1430-find-the-k-beauty-of-a-number/find-the-k-beauty-of-a-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def divisorSubstrings(self, num: int, k: int) -> int:
+        ws = k
+        num = str(num)
+        l = 0
+        cnt = 0
+
+        for i in range(len(num)-(ws-1)):
+            sub = num[i:k+i]
+            if int(sub) == 0:
+                continue
+            if int(num) % int(sub) == 0:
+                cnt +=1
+        return cnt
+
+        
