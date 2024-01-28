45dc9c23693e704a404d218c950a3ab9cab20c92
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-28 12:15:12 +0300

    Time: 821 ms (14.49%) | Memory: 17 MB (65.22%) - LeetSync

---
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+class Solution:
+    def mostFrequentEven(self, nums) -> int:
+        counter = Counter([i for i in nums if i % 2 == 0])
+        if not counter:
+            return -1
+        res = {x: count for x, count in counter.items() if count == max(counter.values())}
+        return min(res.keys())
