ccadd904046fa6185766e3934d9b21a6aaa0cca3
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-28 07:52:39 +0300

    Time: 606 ms (55.10%) | Memory: 17.7 MB (63.67%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.py
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.py
@@ -1,6 +1,6 @@
 class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
-        #tamirar
+        #tamirat
         m, n = len(matrix), len(matrix[0])
         count = 0
 
