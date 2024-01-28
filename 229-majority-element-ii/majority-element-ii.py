a6662380259598ec38271f832f0879a070fa2449
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-28 12:03:59 +0300

    Time: 85 ms (99.45%) | Memory: 17.9 MB (54.90%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        ans = []
+        rep = Counter(nums)
+        checkPoint = len(nums)/3
+        for i, j in rep.items():
+            if j > checkPoint:
+                ans.append(i)
+        return ans
+        
