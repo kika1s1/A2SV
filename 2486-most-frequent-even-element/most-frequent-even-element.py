353dd91b4db064f033fe014618a37d964ce98b13
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-01-28 12:15:45 +0300

    Time: 209 ms (96.99%) | Memory: 17.1 MB (58.53%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.py
+++ 2486-most-frequent-even-element/most-frequent-even-element.py
@@ -1,10 +1,21 @@
-from collections import Counter
-
 class Solution:
-    def mostFrequentEven(self, nums) -> int:
-        #tamirat
-        counter = Counter([i for i in nums if i % 2 == 0])
-        if not counter:
-            return -1
-        res = {x: count for x, count in counter.items() if count == max(counter.values())}
-        return min(res.keys())
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        #TAMIRAT
+        map={}
+        
+        evens=[]
+        res=[]
+        for num in nums:
+            if num%2==0:
+                evens.append(num)
+        for num in evens:
+            if num not in map:
+                map[num]=1
+            else:
+                map[num] +=1
+        if not map : return -1
+        maxi = max(map.values())
+        for k, v in map.items():
+            if v == maxi:
+                res.append(k)
+        return min(res)
