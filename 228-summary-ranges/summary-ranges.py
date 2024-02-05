395ed81d3a69d6e92317df500d394915edad4d66
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-05 20:10:20 +0300

    Time: 33 ms (78.62%) | Memory: 16.5 MB (66.61%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.py
@@ -0,0 +1,22 @@
+class Solution:
+    def summaryRanges(self, nums):
+        if not nums:
+            return []
+
+        ranges = []
+        start = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1] + 1:
+                if start == nums[i-1]:
+                    ranges.append(str(start))
+                else:
+                    ranges.append(str(start) + "->" + str(nums[i-1]))
+                start = nums[i]
+
+        if start == nums[-1]:
+            ranges.append(str(start))
+        else:
+            ranges.append(str(start) + "->" + str(nums[-1]))
+
+        return ranges
