9a42f8970fe61f90f1d8af82eb7822cca603eb2f
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-07 10:43:13 +0300

    Time: 46 ms (83.96%) | Memory: 16.7 MB (71.94%) - LeetSync

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -1,27 +1,14 @@
 class Solution:
     def numOfPairs(self, nums: List[str], target: str) -> int:
         d = defaultdict(int)
-        for char in nums:
-            d[char] += 1
+        result = 0
+        tl = len(target)
+        for chars in nums:
+            l = len(chars)
+            if target[:l] == chars:
+                result += d[target[l:]]
+            if target[tl - l:] == chars:
+                result += d[target[:tl - l]]
+            d[chars] += 1
+        return result
             
-        arr = []
-        for char in target:
-            arr.append(char)
-        
-        pairs = 0
-        num = ""
-        while len(arr) > 1:
-            num += arr.pop()
-            findNum = "".join(arr)
-            if num[::-1] not in d or findNum not in d:
-                continue
-
-            c1 = d[num[::-1]]
-            d[num[::-1]] -= 1 #reduce the count as we dont want to count it again if the other part is also same.
-            
-            c2 = d[findNum]
-            d[num[::-1]] += 1 # make the count again same.
-
-            pairs += c1 * c2
-        return pairs
-        
