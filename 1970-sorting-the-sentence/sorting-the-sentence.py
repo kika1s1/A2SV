25812f2564c97451b31e880b5e315e8524fab7f8
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-20 15:10:10 +0300

    Time: 22 ms (99.12%) | Memory: 16.5 MB (64.82%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -1,7 +1,6 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        ans = [x for x in s.split()]
-        # place
+        ans = s.split()
         for i in s.split():
             place = int(i[-1])-1
             ans[place] = i[:-1]
