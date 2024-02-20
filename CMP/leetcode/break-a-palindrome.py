9b2d56ee55676a0cf5d06bef897792060d2c2631
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-20 16:40:07 +0300

    Add solution for Break a Palindrome

---
+++ CMP/leetcode/break-a-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        copPal = palindrome
+        if len(palindrome) == 1:
+            return ""
+        for i in palindrome:
+            if i != "a":
+                prev = i
+                print(prev)
+                palindrome = palindrome.replace(i, "a", 1)
+                if palindrome == palindrome[::-1]:
+                    palindrome = copPal
+                    continue
+                else:
+                    return palindrome
+        palindrome = palindrome[:-1] + "b"
+        return palindrome
