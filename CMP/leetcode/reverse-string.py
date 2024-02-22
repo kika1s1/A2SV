3b05caf9796c573531006d5fa8f29317087ebb31
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-22 22:04:17 +0300

    Add solution for Reverse String

--- CMP/leetcode/reverse-string.py
+++ CMP/leetcode/reverse-string.py
@@ -1,6 +1,9 @@
 class Solution(object):
     def reverseString(self, s):
-       l=s.reverse()
-       return l
-
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
+        return s
         
