a1c742841a5bc91a6445607baf53b912066fa10a
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-22 22:26:59 +0300

    Add solution for Pow(x, n)

--- CMP/leetcode/powx-n.py
+++ CMP/leetcode/powx-n.py
@@ -1,13 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n < 0:
-            return 1.0 / self.pow(x, -n)
-        return self.pow(x, n)
-    
-    def pow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1.0
-        if n % 2 == 0:
-            return self.pow(x * x, n // 2)
-        else:
-            return x * self.pow(x * x, (n - 1) // 2)
+        res=1
+        if n<0:
+            x=1/x
+            n=-n
+        while n:
+            if n%2==1:
+                res*=x
+            x*=x
+            n//=2
+        return res
+        
