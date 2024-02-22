73239f7933cd730a60a1cc9ec0c4e46f181f281c
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-22 09:46:39 +0300

    Time: 617 ms (37.81%) | Memory: 20.9 MB (99.80%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -2,13 +2,22 @@ class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
         if n == 1:
             return 1
-        candidates = set()
-        anti_candidates = set()
-        trust_counter = [0] * n
-        for t, t1 in trust:
-            trust_counter[t1-1] += 1
-            anti_candidates.add(t)
-            if trust_counter[t1-1] == n-1:
-                candidates.add(t1)
-        candidates = candidates.difference(anti_candidates)
-        return candidates.pop() if candidates else -1
+        trust_Table = {}
+        checkJudgeTable = {}
+        for i, j in trust:
+            trust_Table[i] = j
+            if i in checkJudgeTable:
+                checkJudgeTable[i] = j
+            elif j not in trust_Table:
+                checkJudgeTable[j] = 0
+            else:
+                checkJudgeTable[j] = i
+        cnt = 0
+        value = 0
+        for i, j in checkJudgeTable.items():
+            if j == 0:
+                cnt +=1
+                value = i
+        if cnt > 1 or cnt == 0:
+            return -1
+        return value
