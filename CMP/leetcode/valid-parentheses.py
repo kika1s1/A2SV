1a90e28941b28b5bbd67a871b979372589cb357d
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-22 10:58:47 +0300

    Add solution for Valid Parentheses

--- CMP/leetcode/valid-parentheses.py
+++ CMP/leetcode/valid-parentheses.py
@@ -18,29 +18,4 @@ class Solution(object):
                     return False
         return len(stack) == 0
             
-            
-
-
-        # if len(s) < 2 or s[0] in matches:
-        #     return False
-
-        # stack = []
-
-        # for bracket in s:
-        #     if bracket in "([{":
-        #         stack.append(bracket)
-        #         continue
-
-        #     if len(stack) <= 0:
-        #         return False
-
-        #     if stack[-1] == matches[bracket]:
-        #         stack.pop()
-        #     else:
-        #         return False
-
-        # if len(stack) == 0:
-        #     return True
-        # else:
-        #     return False
-        
+       
