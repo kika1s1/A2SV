4459c994cf8679f0cb9dbce311cc7f13970cde0f
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-23 18:44:44 +0300

    Add solution for K Closest Points to Origin

---
+++ CMP/leetcode/k-closest-points-to-origin.py
@@ -0,0 +1,6 @@
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        points.sort(key=lambda x: ((x[0]**2)+(x[1]**2))**0.5)
+        return points[:k]
