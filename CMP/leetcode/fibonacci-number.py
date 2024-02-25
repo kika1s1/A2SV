e05881f59106ebb7025a9ee33200c71bc7dc899b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-25 14:29:51 +0300

    Add solution for Fibonacci Number

--- CMP/leetcode/fibonacci-number.py
+++ CMP/leetcode/fibonacci-number.py
@@ -1,11 +1,7 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <=1:
-            return n
-        dp = [0]*(n+1)
-        dp[0] = 0 
-        dp[1] = 1 
-        dp[2] = 1
-        for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n] 
+        if n ==0:
+            return 0
+        if n == 1:
+            return 1
+        return self.fib(n-1) + self.fib(n-2)
