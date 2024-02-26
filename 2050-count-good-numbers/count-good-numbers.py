dbd082d3a385f7b20973b0544ad2913dbb598de2
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-26 16:22:11 +0300

    Time: 40 ms (29.50%) | Memory: 16.7 MB (44.60%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,3 +1,3 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        return (pow(5,(n + 1)//2,(1000000007))*pow(4,n//2,1000000007))%1000000007
+        return (pow(5, (n+1)//2,((10**9)+7)))*pow(4, (n//2), ((10**9)+7)) % ((10**9)+7)
