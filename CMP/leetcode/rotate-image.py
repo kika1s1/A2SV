30d9183a1687fcd92d80181e8ca8d00707d8d1dd
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-29 22:21:57 +0300

    Add solution for Rotate Image

---
+++ CMP/leetcode/rotate-image.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def rotate(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
+            # transpose and reverse
