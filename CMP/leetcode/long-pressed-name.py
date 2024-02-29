c8375516726afc224eeaf548c9a5604b23f1b444
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-02-29 22:52:28 +0300

    Add solution for Long Pressed Name

---
+++ CMP/leetcode/long-pressed-name.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+
+
+        i, j = 0, 0
+        while j < len(typed):
+
+            if i < len(name) and name[i] == typed[j]:
+                i += 1
+                j += 1
+            elif j > 0 and typed[j-1] == typed[j]:
+                j += 1
+            else:
+                return False
+
+        return i == len(name)
