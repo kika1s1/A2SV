1a9f653896b78774d2a7304155dd0b7d916b7287
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-02 08:15:59 +0300

    Add solution for Squares of a Sorted Array

--- CMP/leetcode/squares-of-a-sorted-array.py
+++ CMP/leetcode/squares-of-a-sorted-array.py
@@ -1,4 +1,13 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted([x*x for x in nums])
-        
+        ans = []
+        l, r = 0, len(nums)-1
+        while l <= r:
+            if nums[l]**2 > nums[r]**2:
+                ans.append(nums[l]**2)
+                l +=1
+            else:
+                ans.append(nums[r]**2)
+                r -=1
+        ans.reverse()
+        return ans
