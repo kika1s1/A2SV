8dab8c8d6b3d1957d06b15a583cb8e88dad442aa
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-03 15:42:31 +0300

    Add solution for Binary Tree Level Order Traversal

---
+++ CMP/leetcode/binary-tree-level-order-traversal.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = deque()
+        results = []
+        queue.append(root)
+        while queue:
+            qlen = len(queue)
+            level = []
+            for i in range(qlen):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                results.append(level)
+        return results
+
