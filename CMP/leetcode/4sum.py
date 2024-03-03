f1c3621208b387591a6b462c86ed56b856bad2f4
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-03 12:59:11 +0300

    Add solution for 4Sum

--- CMP/leetcode/4sum.py
+++ CMP/leetcode/4sum.py
@@ -1,31 +1,39 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        results = []
-        self.nSum(nums, target, 4, [], results)
-        return results
-    
-    def nSum(self, nums: List[int], target: int, n: int, prefix: List[int], results: List[List[int]]):
-        if len(nums) < n or nums[0] * n > target or nums[-1] * n < target:
-            return
-        
-        if n == 2:
-            left, right = 0, len(nums) - 1
-            while left < right:
-                curr_sum = nums[left] + nums[right]
-                if curr_sum == target:
-                    results.append(prefix + [nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-                elif curr_sum < target:
-                    left += 1
+
+        result=[]
+        quad=[]
+
+        def k_sum(k,start,target):
+            if k!=2:
+                for i in range(start,len(nums)-k+1):
+                    if i>start and nums[i]==nums[i-1]:
+                        continue
+
+                    quad.append(nums[i])
+                    k_sum(k-1,i+1,target-nums[i])
+                    quad.pop()
+
+                return
+            l=start
+            r=len(nums)-1
+
+            while l<r:
+                if nums[l]+nums[r]>target:
+                    r-=1
+
+                elif nums[l]+nums[r]<target:
+                    l+=1
+
                 else:
-                    right -= 1
-        else:
-            for i in range(len(nums) - n + 1):
-                if i == 0 or (i > 0 and nums[i] != nums[i - 1]):
-                    self.nSum(nums[i + 1:], target - nums[i], n - 1, prefix + [nums[i]], results)
+                    result.append(quad+[nums[l],nums[r]])
+                    l+=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+
+
+        k_sum(4,0,target)
+
+
+        return result
