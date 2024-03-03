512c900469e5945bd8183b0f63b37e614b1e0127
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-03 12:25:56 +0300

    Time: 109 ms (39.58%) | Memory: 17.5 MB (47.98%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,19 +1,13 @@
-class Solution(object):
-    def twoSum(self, n, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        first = 0
-        last = len(n)-1
-        while first < last:
-            if n[first] + n[last] == target:
-                return [first+1, last+1]
-            elif n[first] + n[last] < target:
-                first += 1
-            elif n[first] + n[last] > target:
-                last -= 1
-
-            
-        
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while l < r:
+            twoSum = numbers[l] + numbers[r]
+            if twoSum > target:
+                r -=1
+            elif twoSum < target:
+                l +=1
+            else:
+                return ([l+1, r+1])
+               
+        return ans[0]
