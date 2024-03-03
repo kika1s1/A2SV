ed6b36f33cf1971948c439f8fa227ab897e79e45
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-03 14:22:27 +0300

    Time: 94 ms (48.76%) | Memory: 16.5 MB (74.92%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        res = []
+        for i in range(9):
+            for j in range(9):
+                element = board[i][j]
+                if element != '.':
+                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
+        return len(res) == len(set(res))
