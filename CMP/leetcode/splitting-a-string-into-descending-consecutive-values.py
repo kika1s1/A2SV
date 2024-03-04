3cd2465575b24739cd9b80f3739009eb940a9437
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-04 19:26:45 +0300

    Add solution for Splitting a String Into Descending Consecutive Values

--- CMP/leetcode/splitting-a-string-into-descending-consecutive-values.py
+++ CMP/leetcode/splitting-a-string-into-descending-consecutive-values.py
@@ -1,12 +1,12 @@
 class Solution:
     def splitString(self, s: str) -> bool:
-        def backtrack(low, lastNum):
-            for i in range(low, len(s)-1):
-                split = int(s[low: i+1]);
-                if (lastNum == '#' or lastNum-1 == split):
+        def backtrack(start, prev):
+            for i in range(start, len(s) - 1):
+                split = int(s[start: i + 1])
+                if prev == -1 or prev-1 == split:
                     if backtrack(i+1, split):
-                        return True;
-            if lastNum != '#' and int(s[low:]) == lastNum - 1:
-                return True;
-            return False;
-        return backtrack(0, '#');
+                      return  True
+            if prev != -1 and int(s[start:]) == prev - 1:
+                return True
+            return False
+        return backtrack(0, -1)
