edc601287bc51603152d70fee1c0009d110d28fd
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-04 20:58:39 +0300

    Add solution for Toeplitz Matrix

---
+++ CMP/leetcode/toeplitz-matrix.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i >0 and j >0:
+                    if matrix[i][j] != matrix[i-1][j-1]:
+                        return False
+        return True
