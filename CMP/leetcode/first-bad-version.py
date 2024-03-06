bfec58e67557cc2ec1314b6f69e51e635e9ba8e4
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-06 13:16:12 +0300

    Add solution for First Bad Version

--- CMP/leetcode/first-bad-version.py
+++ CMP/leetcode/first-bad-version.py
@@ -3,17 +3,15 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-
         low = 1
         high = n
-
-        while(low<=high):
-            mid = (low+high)//2
-            if(isBadVersion(mid)):
+        while low <= high:
+            mid = low + (high-low)//2
+            if isBadVersion(mid) == False:
+                low = mid + 1
+            elif isBadVersion(mid):
                 high = mid-1
-            elif(isBadVersion(mid)==False):
-                low = mid +1
-            if(mid==0 and isBadVersion(mid)==True):
+            if mid == 0 and isBadVersion(mid) == True:
                 return mid
-            if(mid>0 and isBadVersion(mid)==True and isBadVersion(mid-1)==False):
+            if mid > 0 and isBadVersion(mid) == True and isBadVersion(mid-1) == False:
                 return mid
