6e5c726c415f146d53a285c2067ec3005b897517
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-06 12:58:49 +0300

    Add solution for First Bad Version

---
+++ CMP/leetcode/first-bad-version.py
@@ -0,0 +1,19 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+
+        low = 1
+        high = n
+
+        while(low<=high):
+            mid = (low+high)//2
+            if(isBadVersion(mid)):
+                high = mid-1
+            elif(isBadVersion(mid)==False):
+                low = mid +1
+            if(mid==0 and isBadVersion(mid)==True):
+                return mid
+            if(mid>0 and isBadVersion(mid)==True and isBadVersion(mid-1)==False):
+                return mid
