0e9335414795c3fc82828b1bfaafbb8082cd8b10
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-07 16:46:50 +0300

    Time: 182 ms (93.99%) | Memory: 22.1 MB (55.20%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.py
+++ 969-number-of-recent-calls/number-of-recent-calls.py
@@ -6,3 +6,6 @@ class RecentCounter:
         while self.queue[0] < t - 3000: 
             self.queue.popleft() 
         return len(self.queue)                           
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
