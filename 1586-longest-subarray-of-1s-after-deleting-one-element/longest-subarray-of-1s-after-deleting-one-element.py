8e45b529ffdaed7caac50e19db432a460244122e
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-07 07:55:59 +0300

    Time: 422 ms (90.66%) | Memory: 19.6 MB (84.72%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -1,39 +1,3 @@
-# class Solution:
-#     def longestSubarray(self, nums: List[int]) -> int:
-#         maxim = float("-inf")
-#         for i in range(len(nums)):
-#             cnt = 0
-#             for j in range(len(nums)):
-#                 if j == i:
-#                     continue
-#                 elif nums[j] == 1:
-#                     cnt += 1
-#                     maxim = max(maxim, cnt)
-#                 else:
-#                     cnt = 0
-
-#                     maxim = max(maxim, cnt)
-
-#         return maxim
-
-# class Solution:
-#     def longestSubarray(self, nums: List[int]) -> int:
-#         prev=0
-#         count=0
-#         ls=[]
-#         visit=False
-#         for i in nums:
-#             if i==0:
-#                 visit=True
-#                 ls.append(prev+count)
-#                 prev=count
-#                 count=0
-#             if i==1:
-#                 count+=1
-#         if not visit:
-#             count-=1
-#         ls.append(count+prev)
-#         return max(ls)
 
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
