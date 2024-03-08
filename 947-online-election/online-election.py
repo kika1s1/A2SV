2d164f7b10a45fa97a17cc13c321b022dd19e750
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-08 13:49:12 +0300

    Time: 423 ms (78.09%) | Memory: 22.4 MB (75.71%) - LeetSync

---
+++ 947-online-election/online-election.py
@@ -0,0 +1,26 @@
+class TopVotedCandidate:
+
+    def __init__(self, persons: List[int], times: List[int]):
+        self.persons = []
+        self.times = []
+        self.dic = defaultdict(int)
+        self.m = 0
+        self.idx = -1
+
+        for i in range(len(times)):
+            self.times.append(times[i])
+            self.dic[persons[i]] += 1
+            if self.dic[persons[i]] >= self.m:
+                self.persons.append(persons[i])
+                self.m = self.dic[persons[i]]
+            else:
+                self.persons.append(self.persons[-1])
+
+    def q(self, t: int) -> int:
+        idx = bisect.bisect_right(self.times,t)
+        return self.persons[idx-1]
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
