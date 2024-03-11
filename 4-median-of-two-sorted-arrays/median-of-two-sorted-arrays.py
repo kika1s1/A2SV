00d4d8f484ec923f1b5dc7778a21b71465e660e3
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-11 21:58:41 +0300

    Time: 81 ms (57.66%) | Memory: 16.9 MB (43.64%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -5,11 +5,11 @@ class Solution:
         but guys shame on us since it is hard question let try to write a little bit 
         by ourselves \U0001f601\U0001f601
         """
-        both = nums1+nums2
-        both.sort()
-        # print(both)
-        if len(both)%2 == 1:
-            return both[len(both)//2]
+        nums1.extend(nums2)
+        nums1.sort()
+        # print(nums1)
+        if len(nums1)%2 == 1:
+            return nums1[len(nums1)//2]
         else:
-            # print(both[(len(both)//2)-1], both[len(both)//2])
-            return ( both[(len(both)//2)-1] +  both[len(both)//2])/2
+            # print(nums1[(len(nums1)//2)-1], nums1[len(nums1)//2])
+            return ( nums1[(len(nums1)//2)-1] +  nums1[len(nums1)//2])/2
