052053606601babdbd9495432354e2c66321cb55
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-11 20:37:10 +0300

    Time: 297 ms (18.66%) | Memory: 21.3 MB (55.71%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.py
@@ -0,0 +1,10 @@
+class StockSpanner:
+    def __init__(self):
+        self.stack = []
+
+    def next(self, price: int) -> int:
+        cnt = 1
+        while self.stack and self.stack[-1][0] <= price:
+            cnt += self.stack.pop()[1]
+        self.stack.append((price, cnt))
+        return cnt
