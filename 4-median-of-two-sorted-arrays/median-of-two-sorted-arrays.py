fd034e8a83659f1f8326eac9677b1b47f0acfbc7
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-03-11 21:56:24 +0300

    Time: 62 ms (99.43%) | Memory: 16.9 MB (43.64%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,3 +1,15 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        return median(sorted(nums1 + nums2))
+        """
+        I see leetcode account return median(sorted(nums1 + nums2)) 
+        but guys shame on us since it is hard question let try to write a little bit 
+        by ourselves \U0001f601\U0001f601
+        """
+        both = nums1+nums2
+        both.sort()
+        # print(both)
+        if len(both)%2 == 1:
+            return both[len(both)//2]
+        else:
+            # print(both[(len(both)//2)-1], both[len(both)//2])
+            return ( both[(len(both)//2)-1] +  both[len(both)//2])/2
