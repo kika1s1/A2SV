6cf5d2e2c3a905374003d873ee4cac7de8c0bcbc
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-04-19 17:20:29 +0300

    Created 'Keys and Rooms' by A2SV Hub

---
+++ 19-Apr-2024/Keys and Rooms 127735.py
@@ -0,0 +1,13 @@
+# Problem: Keys and Rooms - https://leetcode.com/problems/keys-and-rooms/
+
+class Solution:
+   def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = set()
+        def dfs(room):
+            if room in visited:
+                return
+            visited.add(room)
+            for key in rooms[room]:
+                dfs(key)
+        dfs(0)
+        return len(visited) == len(rooms)
