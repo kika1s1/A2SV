596e29a71d870374f27b30adf8691ab15020fbc6
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-04-23 21:59:11 +0300

    Time: 95 ms (32.78%) | Memory: 18 MB (97.16%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,33 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        in_degrees = [0] * numCourses
+        
+        for prerequisite in prerequisites:
+            course, prerequisite_course = prerequisite
+            graph[prerequisite_course].append(course)
+            in_degrees[course] += 1
+        
+        # Step 2: Perform topological sorting using BFS
+        queue = deque()
+        for course in range(numCourses):
+            if in_degrees[course] == 0:
+                queue.append(course)
+        
+        order = []
+        while queue:
+            prerequisite_course = queue.popleft()
+            order.append(prerequisite_course)
+            
+            for course in graph[prerequisite_course]:
+                in_degrees[course] -= 1
+                if in_degrees[course] == 0:
+                    queue.append(course)
+        
+        if len(order) != numCourses:
+            return []
+        
+        return order
