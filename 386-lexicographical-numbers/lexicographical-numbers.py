6e270305060541d263f82df89a27062e418bfb09
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-04-24 09:52:30 +0300

    Time: 78 ms (50.21%) | Memory: 24.8 MB (27.27%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,9 +1,26 @@
+# class Solution:
+#     def lexicalOrder(self, n: int) -> List[int]:
+#         heap = []
+#         for i in range(1,n+1):
+#             heappush(heap, str(i))
+#         ans = []
+#         while heap:
+#             ans.append(int(heappop(heap)))
+#         return ans
+
+
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        heap = []
-        for i in range(1,n+1):
-            heappush(heap, str(i))
         ans = []
-        while heap:
-            ans.append(int(heappop(heap)))
+        
+        def recursive(value):
+            if len(ans) >= n:
+                return
+            ans.append(value)
+            if (value * 10) <= n:
+                recursive(value * 10)
+            if value + 1 <= n and int(str(value)[-1]) < 9:
+                recursive(value + 1)
+        recursive(1)
+
         return ans
