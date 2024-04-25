f7aaef337570d79118b62b192fabb6dbf5761543
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-04-25 20:04:24 +0300

    Created 'Checking Existence of Edge Length Limited Paths' by A2SV Hub

---
+++ 25-Apr-2024/Checking Existence of Edge Length Limited Paths 133628.py
@@ -0,0 +1,40 @@
+# Problem: Checking Existence of Edge Length Limited Paths - https://leetcode.com/problems/checking-existence-of-edge-length-limited-paths/
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if x_root == y_root:
+            return
+        if self.rank[x_root] < self.rank[y_root]:
+            self.parent[x_root] = y_root
+        elif self.rank[x_root] > self.rank[y_root]:
+            self.parent[y_root] = x_root
+        else:
+            self.parent[y_root] = x_root
+            self.rank[x_root] += 1
+
+class Solution:
+    def distanceLimitedPathsExist(self, n: int, edgeList: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        edgeList.sort(key=lambda x: x[2])
+        queries = sorted(enumerate(queries), key=lambda x: x[1][2])
+
+        uf = UnionFind(n)
+        res = [False] * len(queries)
+        i = 0
+        for j, (u, v, limit) in queries:
+            while i < len(edgeList) and edgeList[i][2] < limit:
+                uf.union(edgeList[i][0], edgeList[i][1])
+                i += 1
+            res[j] = uf.find(u) == uf.find(v)
+
+        return res
