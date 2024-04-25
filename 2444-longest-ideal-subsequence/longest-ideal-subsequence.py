6454929dec315978414c419bcdea2d8cdeac7b9d
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-04-25 10:03:46 +0300

    Time: 309 ms (93.33%) | Memory: 17.3 MB (68.15%) - LeetSync

---
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def longestIdealString(self, s: str, k: int) -> int:
+        l = [0] * 128
+        for c in s:
+            i = ord(c)
+            l[i] = max(l[i - k : i + k + 1]) + 1
+        return max(l)
