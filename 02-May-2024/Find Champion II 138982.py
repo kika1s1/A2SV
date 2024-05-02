1141d600a25a87e8a6e8c20079b9b6d4fe1e37c1
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-02 11:00:37 +0300

    Created 'Find Champion II' by A2SV Hub

---
+++ 02-May-2024/Find Champion II 138982.py
@@ -0,0 +1,31 @@
+# Problem: Find Champion II - https://leetcode.com/problems/find-champion-ii/
+
+from collections import defaultdict
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        if len(edges) == 0:
+            if n == 1:
+                return 0
+            else:
+                return -1
+        
+        graph = {}
+        for u, v in edges:
+            if u not in graph:
+                graph[u] = []
+            graph[u].append(v)
+        
+        def dfs(graph, visited, node):
+            visited.add(node)
+            for neighbour in graph.get(node, []):
+                if neighbour not in visited:
+                    dfs(graph, visited, neighbour)
+        
+        for u, v in edges:
+            visited = set()
+            dfs(graph, visited, u)
+            if len(visited) == n:
+                return u
+        
+        return -1
