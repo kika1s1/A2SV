1dbc9fc6e7fbe5fd9281c2b679ffbc46acc05d91
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-03 11:54:46 +0300

    Time: 53 ms (7.38%) | Memory: 16.5 MB (97.89%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.py
@@ -1,39 +1,28 @@
-class Disjoint:
-    def __init__(self,n):
-        self.size=[1]*n
-        self.parent=[i for i in range(n)]
-
-    def findUPar(self,node):
-        if node==self.parent[node]:
-            return node
-        self.parent[node]=self.findUPar(self.parent[node])
-        return self.parent[node]
-
-    def union(self,u,v):
-        ulp_u=self.findUPar(u)
-        ulp_v=self.findUPar(v)
-        if ulp_u==ulp_v:
-            return
-        if ulp_u<ulp_v:
-            self.parent[ulp_v]=ulp_u
-        elif ulp_u>ulp_v:
-            self.parent[ulp_u]=ulp_v
-            
-
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        disjoint=Disjoint(26)
-        n=len(s1)
-        for i in range(n):
-            # print(s1[i],s2[i],ord(s1[i])-97,ord(s2[i])-97)
-            disjoint.union(ord(s1[i])-97,ord(s2[i])-97)
-        
-        st=""
-        for i in range(26):
-            disjoint.findUPar(i)
-        # print(disjoint.parent)
+        # union find by Rank 
 
-        for i in baseStr:
-            x=disjoint.findUPar(ord(i)-97)
-            st+=chr(x+97)
-        return st
+        parent = {a : a for a in s1 + s2}
+        
+        def find(node):
+            while node != parent[node]:
+                parent[node] = parent[parent[node]]
+                node = parent[node]
+            return node
+        def union(node1, node2):
+            p1, p2 = find(node1), find(node2)
+            if p1 < p2:
+                parent[p2] = p1
+            else:
+                parent[p1] = p2
+        for a, b in zip(s1, s2):
+            union(a, b)
+        ans = ""
+        for s in baseStr:
+            if s in parent:
+                ans += find(s)
+            else:
+                ans += s
+        return ans
+
+        
