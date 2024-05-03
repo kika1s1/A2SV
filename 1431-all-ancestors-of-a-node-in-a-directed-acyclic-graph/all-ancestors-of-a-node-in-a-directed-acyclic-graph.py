94a33515bd601d709bcf133c3bb67d770b4264be
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-03 10:47:32 +0300

    Time: 440 ms (90.84%) | Memory: 48 MB (46.87%) - LeetSync

---
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        graph = defaultdict(list)
+        in_degree = defaultdict(int)
+
+        for u, v in edges:
+            graph[u].append(v)
+            in_degree[v] +=1
+        
+        d = deque([node for node in range(n) if node not in in_degree])
+        res = [set() for _ in range(n)]
+        while d:
+            node = d.popleft()
+            for child in graph[node]:
+                res[child].add(node)
+                res[child].update(res[node])
+                in_degree[child] -=1
+                if in_degree[child] == 0:
+                    d.append(child)
+        return [sorted(x) for x in res] 
