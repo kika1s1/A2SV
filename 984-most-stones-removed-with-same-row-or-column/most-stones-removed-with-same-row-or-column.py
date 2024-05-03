4bdc2e01a44334f9644fe449eb13ec36942a871b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-03 11:50:58 +0300

    Time: 176 ms (39.28%) | Memory: 18.8 MB (12.26%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.py
@@ -1,38 +1,31 @@
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        hash_row = defaultdict(list)
-        hash_col = defaultdict(list)
-        
-        parent =  {}
-        for i,stone in enumerate(stones):
-            x,y= stone[0],stone[1]
-            hash_row[x].append((x,y))
-            hash_col[y].append((x,y))
-            stones[i]= tuple(stone)
-            parent[tuple(stone)]= tuple(stone)
-        
-        def find(x,y):
-            if parent[(x,y)]!=(x,y):
-                parent[(x,y)]=find(parent[(x,y)][0],parent[(x,y)][1])
-            return parent[(x,y)]
+        #
+        parent = {}
+        row = collections.defaultdict(list)
+        col = collections.defaultdict(list)
 
-        def union(x_1,y_1,x_2,y_2):
-            r1= find(x_1,y_1)
-            r2 = find(x_2,y_2)
-            if r1!=r2:
-                parent[r1]=r2
-        for row in hash_row.keys():
-            row = hash_row[row]
-            p1 = row[0]
-            for p2 in row[1:]:
-                union(p1[0],p1[1],p2[0],p2[1])
-        for col in hash_col.keys():
-            col = hash_col[col]
-            p1 = col[0]
-            for p2 in col[1:]:
-                union(p1[0],p1[1],p2[0],p2[1])
-        group = set()                
-        for point in parent.keys():
-            cluster_root = find(point[0],point[1])
-            group.add(cluster_root)
-        return len(stones)-len(group)
+        def find(x):
+            if parent[x]!=x:
+                return find(parent[x])
+
+            return x
+
+        def union(a, b):
+            if a!=b:
+                parent[find(a)]=find(b)
+            
+        for i, j in stones:
+            parent[(i,j)] = (i, j)
+            row[i].append((i, j))
+            col[j].append((i, j))
+        
+        for i in row:
+            for x in row[i]:
+                union(row[i][0], x)
+        for j in col:
+            for x in col[j]:
+                union(col[j][0], x)
+        
+        unions = set(find((i, j)) for i , j in stones)
+        return len(stones)-len(unions)
