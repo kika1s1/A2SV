089c5d214a0d0f5300f31f2acb06092d6c1e0b31
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-03 08:39:59 +0300

    Time: 34 ms (63.26%) | Memory: 16.4 MB (91.99%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,18 +1,25 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        com1 = [int(x) for x in version1.split(".")]
-        com2 = [int(x) for x in version2.split(".")]
-        loop = len(com1)-len(com2)
-        if loop < 0:
-            for i in range(abs(loop)):
-                com1.append(0)
-        else:
-            for i in range(abs(loop)):
-                com2.append(0)
-
-        if com1 < com2:
-            return -1
-        elif com1>com2:
+        v1 = [int(x) for x in version1.split(".")]
+        v2 = [int(x) for x in version2.split(".")]
+        for i in v1[::-1]:
+            if i == 0:
+                v1.pop()
+            else:
+                break
+        for i in v2[::-1]:
+            if i ==0:
+                v2.pop()
+            else:
+                break
+        for i, j in zip(v1, v2):
+            if i < j:
+                return -1
+            elif i >j:
+                return 1
+        if len(v1) > len(v2):
             return 1
+        if len(v1) < len(v2):
+            return -1
         else:
             return 0
