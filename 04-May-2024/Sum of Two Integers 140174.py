816cb1c762b7fc7ee9896ee7e421681cd70d14fd
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-04 13:05:53 +0300

    Created 'Sum of Two Integers' by A2SV Hub

---
+++ 04-May-2024/Sum of Two Integers 140174.py
@@ -0,0 +1,6 @@
+# Problem: Sum of Two Integers - https://leetcode.com/problems/sum-of-two-integers/description/
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # i did not used + indeed \U0001f601\U0001f601
+        return sum([a,b])
