48baa1607ce73df424c0d38eb03159c580e1ded6
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-04 09:45:31 +0300

    Time: 59 ms (68.05%) | Memory: 16.7 MB (49.85%) - LeetSync

--- 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.py
+++ 3399-find-the-integer-added-to-array-ii/find-the-integer-added-to-array-ii.py
@@ -1,19 +1,43 @@
 class Solution:
-    def minimumAddedInteger(self, nums1: List[int], nums2: List[int]) -> int:
-        def find_diff(i, j):
-            num = nums1.copy()
-            num.pop(i)
-            num.pop(j-1)
-            diff = nums2[0]-num[0]
-            for a, b in zip(num,nums2):
-                if diff !=b-a:
-                    return float("inf")
-            return diff
+    def minimumAddedInteger(self, nums1, nums2):
+        n = len(nums1)
 
-        nums1.sort()
-        nums2.sort()
-        minim = float("inf")
-        for i in range(len(nums1)):
-            for j in range(i+1, len(nums1)):
-                minim = min(minim, find_diff(i, j))
-        return minim
+        min1_1, min1_2, min1_3 = float('inf'), float('inf'), float('inf')
+        for val in nums1:
+            if val < min1_1:
+                min1_3 = min1_2
+                min1_2 = min1_1
+                min1_1 = val
+            elif val < min1_2:
+                min1_3 = min1_2
+                min1_2 = val
+            elif val < min1_3:
+                min1_3 = val
+
+        min2 = float('inf')
+        for val in nums2:
+            min2 = min(min2, val)
+
+        candidates = [min2 - min1_1, min2 - min1_2, min2 - min1_3]
+
+        numberOccurs = [0] * 1001
+        for i in range(n):
+            n1 = nums1[i]
+            numberOccurs[n1] += 1
+
+        minX = float('inf')
+        for possibleX in candidates:
+            numberCounts = [0] * 1001
+            isPossible = True
+            for i in range(n - 2):
+                target = nums2[i] - possibleX
+                if target < 0 or target > 1000 or numberOccurs[target] == 0 or numberCounts[target] == numberOccurs[target]:
+                    isPossible = False
+                    break
+                else:
+                    numberCounts[target] += 1
+
+            if isPossible:
+                minX = min(minX, possibleX)
+
+        return minX
