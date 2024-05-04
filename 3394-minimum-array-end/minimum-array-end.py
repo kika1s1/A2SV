030622f1cbdb86bea6fd183656db2e8c690961bc
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-04 14:05:41 +0300

    Time: 43 ms (23.58%) | Memory: 16.5 MB (62.98%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.py
+++ 3394-minimum-array-end/minimum-array-end.py
@@ -1,12 +1,19 @@
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        n -= 1
-        i = 0 
-        while n: 
-            while x & 1<<i:
-                i += 1
-            if n & 1:
-                x ^= 1 << i
-            n >>= 1
-            i += 1
-        return x 
+        # make b into every bit not set in x
+
+        b=1
+        n-=1
+        ret=x
+        while n:
+            if b&x==0:
+                if n&1:
+                    ret|=b
+                n>>=1
+            b<<=1
+
+
+        
+
+        return ret
+        
