d33e1b56331e6e1100b348a40c3f7cd7d6dc0860
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-05 10:01:03 +0300

    Time: 384 ms (39.56%) | Memory: 40.6 MB (20.49%) - LeetSync

---
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        temp = head
+        for i in range(k):
+            prev = head
+            head = head.next
+        f = prev
+        slow = temp
+        fast = head
+        while fast:
+            slow = slow.next
+            fast = fast.next
+        f.val, slow.val = slow.val, f.val
+        return temp
+        
