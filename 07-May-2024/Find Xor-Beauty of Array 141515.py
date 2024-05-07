10c68c100e1908176bd7e765d407b8475ec94719
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-07 14:48:45 +0300

    Created 'Find Xor-Beauty of Array' by A2SV Hub

---
+++ 07-May-2024/Find Xor-Beauty of Array 141515.py
@@ -0,0 +1,8 @@
+# Problem: Find Xor-Beauty of Array - https://leetcode.com/problems/find-xor-beauty-of-array/
+
+class Solution:
+    def xorBeauty(self, nums: List[int]) -> int:
+        ans = 0
+        for i in nums:
+            ans ^= i
+        return ans
