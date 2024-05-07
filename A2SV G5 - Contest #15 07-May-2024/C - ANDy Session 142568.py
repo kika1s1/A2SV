87734172436d0e63052daa687b235b7d61622202
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-07 18:10:46 +0300

    Created 'C - ANDy Session' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/C - ANDy Session 142568.py
@@ -0,0 +1,22 @@
+# Problem: C - ANDy Session - https://codeforces.com/gym/522079/problem/C
+
+t = int(input())  
+for _ in range(t):
+    n, k = map(int, input().split())  
+    data = list(map(int, input().split())) 
+    
+    cnt = [0] * 32 
+    for d in data:
+        for i in range(32):
+            cnt[i] += (1 - ((d >> i) & 1))
+    for i in range(30, -1, -1):
+        if k >= cnt[i]:
+            k -= cnt[i]
+            cnt[i] = 0
+    
+    ans = 0  
+    for i in range(32):
+        if cnt[i] == 0:
+            ans += (1 << i)
+    
+    print(ans)
