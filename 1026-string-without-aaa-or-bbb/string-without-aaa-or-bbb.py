d2f59132da9dd04bc971118a5df796b5f336d21f
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-07 10:50:58 +0300

    Time: 35 ms (58.52%) | Memory: 16.6 MB (55.46%) - LeetSync

--- 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.py
+++ 1026-string-without-aaa-or-bbb/string-without-aaa-or-bbb.py
@@ -1,32 +1,19 @@
 class Solution:
     def strWithout3a3b(self, a: int, b: int) -> str:
-        s = ""
-        while a > 0 or b > 0:
-            if a > b:
-                if a > 0:
-                    s += "a"
-                    a -= 1
-                if a > 0:
-                    s += "a"
-                    a -= 1
-                if b > 0:
-                    s += "b"
-                    b -= 1
-            elif b > a:
-                if b > 0:
-                    s += "b"
-                    b -= 1
-                if b > 0:
-                    s += "b"
-                    b -= 1
-                if a > 0:
-                    s += "a"
-                    a -= 1
+        res = ""
+        
+        while a or b:
+            if res[-2:] == "aa":
+                res += "b"
+                b -= 1
+            elif res[-2:] == "bb":
+                res += "a"
+                a -= 1
+            elif a > b:
+                res += "a"
+                a -= 1
             else:
-                if a > 0:
-                    s += "a"
-                    a -= 1
-                if b > 0:
-                    s += "b"
-                    b -= 1
-        return s
+                res += "b"
+                b -= 1
+        
+        return res
