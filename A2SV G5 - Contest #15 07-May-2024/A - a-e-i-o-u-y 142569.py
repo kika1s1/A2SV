9512638b53d3be482d14b450aeac3a8e64312ad7
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-07 18:11:01 +0300

    Created 'A - a-e-i-o-u-y' by A2SV Hub

---
+++ A2SV G5 - Contest #15 07-May-2024/A - a-e-i-o-u-y 142569.py
@@ -0,0 +1,12 @@
+# Problem: A - a-e-i-o-u-y - https://codeforces.com/gym/522079/problem/A
+
+vowel = { "a", "e", "i", "o", "u", "y"}
+word = []
+n = int(input())
+s = input()
+for i in s:
+    if word and word[-1] in vowel and i in vowel:
+        continue
+    else:
+        word.append(i)
+print("".join(word))
