259cc274bedf360b5511b32f6a8c067911a07d5c
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-10 22:59:48 +0300

    Time: 37 ms (90.91%) | Memory: 16.7 MB (96.87%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        sold = 0
+        hold = float('-inf') 
+        rest = 0 
+        for price in prices:   
+            prev_sold = sold
+            sold = hold + price
+            hold = max(hold, rest - price)
+            rest = max(rest, prev_sold)
+        return max(sold, rest)
