af226dfdb9a66ced556ad8fe936fbb719cbe346f
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-13 01:17:07 +0300

    Time: 35 ms (62.72%) | Memory: 16.6 MB (6.97%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.py
+++ 216-combination-sum-iii/combination-sum-iii.py
@@ -1,18 +1,18 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        ans = []
-        checked = []
-        def backtrack(curr):
-            if len(curr) ==k and sum(curr) == n and set(curr) not in checked:
-                checked.append(set(curr))
-                ans.append(curr[:])
-            if len(curr) > k or sum(curr) > n:
-                return 
-            for i in range(1, 10):
-                if  i in curr:
-                    continue
-                curr.append(i)
-                backtrack(curr)
-                curr.pop()
-        backtrack([])
-        return ans
+        
+        def backtrack(i, start, sm, path):
+            if i == k and sm == n:
+                res.append(path[:])
+                return
+
+            for j in range(start, 10):
+                if sm + j <= n:
+                    path.append(j)
+                    backtrack(i + 1, j + 1, sm + j, path)
+                    path.pop()
+                else:
+                    break
+        res = []
+        backtrack(0, 1, 0, [])
+        return res
