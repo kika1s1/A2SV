32eb1550771f2649e45e4de094c2e6e4b2965eaf
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-13 07:30:33 +0300

    Time: 41 ms (56.47%) | Memory: 16.5 MB (92.67%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.py
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.py
@@ -1,34 +1,10 @@
 class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         COLS, ROWS = len(grid[0]), len(grid)
-        for i in range(ROWS):
-            for k in range(COLS):
-                grid[i][k] = str(grid[i][k])
-            if grid[i][0] == "0":
-                for j in range(COLS):
-                    grid[i][j] = "1" if grid[i][j] == "0" else "0"
-        cols_change = set()
-        for i in range(COLS):
-            cnt = 0
-            for j in range(ROWS):
-                if grid[j][i] =="0":
-                    cnt +=1
-            if ROWS-cnt < cnt:
-                cols_change.add(i)
-        for i in range(ROWS):
-            for j in range(COLS):
-                if j in cols_change:
-                    grid[i][j] = "1" if grid[i][j] == "0" else "0"
-        
-        total = 0
-        for row in grid:
-            total +=int("".join(row), 2)
 
+        total = ROWS * (1 << (COLS - 1))
+        for j in range(1, COLS):
+            ones = sum(grid[i][j] == grid[i][0] for i in range(ROWS))
+            flips = max(ones, ROWS - ones)
+            total += flips * (1 << (COLS - 1 - j)) 
         return total
-
-
-                    
-
-            
-                
-
