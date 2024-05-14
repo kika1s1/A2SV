e1ad87b9686bb9169478f5d2ff807d95ca380ad2
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-14 15:10:51 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 14-May-2024/Unique Paths 149209.py
@@ -0,0 +1,15 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp =[[1 for i in range(n)]  for i in range(m)]
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+        
+
+
+
+        # return dfs(0, 0)
+        print(dp)
+        return dp[m-1][n-1]
