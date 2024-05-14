db2e390fef24a7d2e63966d135038764cfc854e5
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-14 15:58:56 +0300

    Time: 38 ms (48.80%) | Memory: 16.5 MB (87.97%) - LeetSync

--- 343-integer-break/integer-break.py
+++ 343-integer-break/integer-break.py
@@ -1,19 +1,11 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
-            # dp = [0] * (n + 1)
-            # dp[1] = 1
-            # dp[2] = 1
-            # for i in range(3, n + 1):
-            #     for j in range(1, i):
-            #         dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]))
-            # return dp[n]
-            if n <=3:
-                return n- 1
-            d3 = n//3
-            r3 = n % 3
-            if r3 == 0:
-                return 3**d3
-            if r3 ==1:
-                r3 = 4
-                d3 -=1
-            return r3 * (3**d3)
+        if n <= 3:
+            return n - 1
+        quotient, remainder = divmod(n, 3)
+        if remainder == 0:
+            return 3 ** quotient
+        elif remainder == 1:
+            return 3 ** (quotient - 1) * 4
+        else:
+            return 3 ** quotient * 2
