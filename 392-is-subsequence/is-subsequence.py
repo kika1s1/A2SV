218b0d698860f821dd80f5d29b469fa736d72b9d
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-17 17:36:53 +0300

    Time: 59 ms (6.01%) | Memory: 20.1 MB (11.83%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,10 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         dp = [{} for i in range(len(t) + 1)]
-        print(dp)
         for i in range(len(t) - 1, -1, -1):
             dp[i] = dp[i + 1].copy()
             dp[i][t[i]] = i + 1
+        print(dp)
         i = 0
         for c in s:
             if c in dp[i]:
