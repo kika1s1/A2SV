7d4442ab0fe76ee50028d7a1c1534d4006c64f0b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-17 22:37:49 +0300

    Created 'Combination Sum IV' by A2SV Hub

---
+++ 17-May-2024/Combination Sum IV 153676.py
@@ -0,0 +1,22 @@
+# Problem: Combination Sum IV - https://leetcode.com/problems/combination-sum-iv/description/
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        memo = {}
+        
+        def backtrack(target):
+            if target in memo:
+                return memo[target]
+            
+            if target == 0:
+                return 1
+            
+            count = 0
+            for num in nums:
+                if target >= num:
+                    count += backtrack(target - num)
+            
+            memo[target] = count
+            return count
+        
+        return backtrack(target)
