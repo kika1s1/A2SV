e70887e7714a0e904dfadedeea03462e53db7f84
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-21 16:34:47 +0300

    Time: 522 ms (63.09%) | Memory: 19.3 MB (73.22%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -1,30 +1,17 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        if not matrix:
-            return 0
+        ROWS, COLS = len(matrix), len(matrix[0])
+        side = 0
+        dp = [[0 for _ in range(COLS)] for _ in range(ROWS)]
         
-        rows, cols = len(matrix), len(matrix[0])
-        memo = [[-1] * cols for _ in range(rows)]
+        for i in range(ROWS):
+            for j in range(COLS):
+                if i == 0 or j == 0:
+                    if matrix[i][j] == "1":
+                        dp[i][j] = 1
+                else:
+                    if matrix[i][j] == "1":
+                        dp[i][j] = min(dp[i-1][j], dp[i-1][j-1], dp[i][j-1]) + 1
+                side = max(side, dp[i][j])
         
-        def dfs(r, c):
-            if r < 0 or c < 0 or r >= rows or c >= cols:
-                return 0
-            if memo[r][c] != -1:
-                return memo[r][c]
-            
-            if matrix[r][c] == "1":
-                right = dfs(r, c+1)
-                bottom = dfs(r+1, c)
-                diag = dfs(r+1, c+1)
-                memo[r][c] = 1 + min(right, bottom, diag)
-            else:
-                memo[r][c] = 0
-            
-            return memo[r][c]
-        
-        max_side = 0
-        for i in range(rows):
-            for j in range(cols):
-                max_side = max(max_side, dfs(i, j))
-        
-        return max_side * max_side
+        return side * side
