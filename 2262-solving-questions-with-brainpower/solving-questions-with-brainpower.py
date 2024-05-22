253e90348c582e565da429bf0e2a807dffbf128b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-22 23:54:52 +0300

    Time: 1365 ms (8.73%) | Memory: 135 MB (11.53%) - LeetSync

---
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        n=len(questions)
+        best=[0]*(n+1)
+        @cache
+        def go(i):
+            if i>=n:
+                return 0
+
+            best=0
+            best=max(go(i+1),best)
+            best=max(go(i+questions[i][1]+1)+questions[i][0],best)
+            return best
+
+
+        return go(0)      
