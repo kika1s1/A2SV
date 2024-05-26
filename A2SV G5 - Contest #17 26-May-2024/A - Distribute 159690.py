c4ceec262d064279318784509d4922f8e28f97ff
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-26 17:14:11 +0300

    Created 'A - Distribute' by A2SV Hub

---
+++ A2SV G5 - Contest #17 26-May-2024/A - Distribute 159690.py
@@ -0,0 +1,13 @@
+# Problem: A - Distribute - https://codeforces.com/gym/524965/problem/A
+
+n = int(input())
+data = list(map(int, input().split()))
+total = sum(data)
+pos  = total//(n//2)
+for i in range(n):
+    for j in range(n):
+        if i !=j:
+            if data[i] + data[j] == pos:
+                print(i+1,j+1)
+                data[i] = float("inf")
+                data[j] = float("inf")
