786a801587b4d743f0b555b92d8db677915d42d5
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-26 17:17:53 +0300

    Created 'A - Non-Identity Permutations' by A2SV Hub

---
+++ A2SV G5 - Contest #16 26-May-2024/A - Non-Identity Permutations 159693.py
@@ -0,0 +1,17 @@
+# Problem: A - Non-Identity Permutations - https://codeforces.com/gym/523525/problem/A
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    data = [i for i in range(1, n+1)]
+    l, r = 0, 1
+    while r < len(data):
+        data[l], data[r] = data[r], data[l]
+        l +=2
+        r +=2
+    if n %2 == 0:
+        print(*data)
+    else:
+        data[0], data[-1] = data[-1], data[0]
+        print(*data)
+
