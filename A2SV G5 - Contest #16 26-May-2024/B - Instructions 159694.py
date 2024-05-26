bcaf920970948084ba3070fb10b8ce72b1f1df77
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-26 17:18:01 +0300

    Created 'B - Instructions' by A2SV Hub

---
+++ A2SV G5 - Contest #16 26-May-2024/B - Instructions 159694.py
@@ -0,0 +1,22 @@
+# Problem: B - Instructions - https://codeforces.com/gym/523525/problem/B
+
+
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().split()))
+        maxim = float("-inf")
+        dp = [0] * n  
+        for i in range(n-1, -1, -1):
+            if i + arr[i] >= n:
+                dp[i] = arr[i] 
+            else:
+                dp[i] = arr[i] + dp[i + arr[i]]  
+            maxim = max(maxim, dp[i])
+
+        print(maxim)
+
+main()
