cb00d7b1781f7dde1b06c7dff097db1f33441569
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-26 17:14:10 +0300

    Created 'B - Grid Path' by A2SV Hub

---
+++ A2SV G5 - Contest #17 26-May-2024/B - Grid Path 159688.py
@@ -0,0 +1,10 @@
+# Problem: B - Grid Path - https://codeforces.com/gym/524965/problem/B
+
+t = int(input())
+
+for i in range(t):
+    n, m, k = map(int, input().split())
+    if n * m - 1 == k:
+        print("YES")
+    else:
+        print("NO")
