212a37040fafc4ea4d27d6df6e0812aee25d99cc
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-26 17:18:22 +0300

    Created 'C - FunkyLlama's Ribbon Challenge' by A2SV Hub

---
+++ A2SV G5 - Contest #16 26-May-2024/C - FunkyLlama's Ribbon Challenge 159695.py
@@ -0,0 +1,20 @@
+# Problem: C - FunkyLlama's Ribbon Challenge - https://codeforces.com/gym/523525/problem/C
+
+def max_ribbon_pieces(n, a, b, c):
+    dp = [-float('inf')] * (n + 1)
+    dp[0] = 0
+
+    for i in range(1, n + 1):
+        if i >= a:
+            dp[i] = max(dp[i], dp[i - a] + 1)
+        if i >= b:
+            dp[i] = max(dp[i], dp[i - b] + 1)
+        if i >= c:
+            dp[i] = max(dp[i], dp[i - c] + 1)
+    
+    return dp[n]
+
+
+n, a, b, c = map(int, input().split())
+
+print(max_ribbon_pieces(n, a, b, c))
