529c47a68a882a5a581fb7f6ea77acc8a6c2d397
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-27 04:58:48 +0300

    Time: 107 ms (77.78%) | Memory: 18.6 MB (6.90%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        b = n  
+        pos = set(nums)
+        for i in range(max(nums)+2):
+            if i not in pos:
+                return i
 
-        for i in range(n):
-            b ^= i ^ nums[i]
-
-        return b
