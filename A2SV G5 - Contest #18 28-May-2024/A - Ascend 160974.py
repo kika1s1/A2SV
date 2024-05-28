82a0bf9977821bb872427fba821e08dac4b8da3d
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-28 17:28:39 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 28-May-2024/A - Ascend 160974.py
@@ -0,0 +1,13 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+n = int(input())
+data = list(map(int, input().split()))
+cnt = 1
+ans = 1
+for i in range(len(data)-1):
+    if data[i] < data[i+1]:
+        cnt +=1
+        ans = max(cnt, ans)
+    else:
+        cnt =1
+print(ans)
