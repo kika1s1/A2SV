36136dd577ba3535309b783757925dbc5340a9aa
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-28 17:28:54 +0300

    Created 'B - String Fusion' by A2SV Hub

---
+++ A2SV G5 - Contest #18 28-May-2024/B - String Fusion 160976.py
@@ -0,0 +1,48 @@
+# Problem: B - String Fusion - https://codeforces.com/gym/526229/problem/B
+
+class Trienode:
+    def __init__(self):
+    	self.child= {}
+    	self.cnt = 0
+    
+   	 
+class Trie:
+    def __init__(self):
+        self.root=Trienode()
+   	 
+    def insert(self, word: str) -> None:
+        cur = self.root
+        
+        for c in word:
+            ind=ord(c)-ord("a")
+
+            if ind  not in cur.child:
+                cur.child[ind]=Trienode()
+            
+            cur=cur.child[ind]
+            cur.cnt += 1
+   	 
+   	 
+    def search(self, word: str) -> int:
+   	 
+        cur=self.root
+        tot = 0
+        for c in word:
+            ind=ord(c)-ord("a")
+            if ind not in cur.child:
+                return tot
+            
+            cur=cur.child[ind]
+            tot += cur.cnt * 2
+
+        return tot
+
+n = int(input())
+s = [input() for i in range(n)]
+trie = Trie()
+for cur in s:
+	trie.insert(cur)
+total = sum([len(cur)*2*n for cur in s])
+for cur in s:
+	total -= trie.search(cur[::-1])
+print(total)
