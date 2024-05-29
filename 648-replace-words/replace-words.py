6d94a7c133f42e1e306c135d0486bd5c5f96f798
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-05-29 14:52:37 +0300

    Time: 115 ms (58.28%) | Memory: 36.7 MB (16.56%) - LeetSync

---
+++ 648-replace-words/replace-words.py
@@ -0,0 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = [None]*26
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        curr = self.root
+        for chr in word:
+            index = ord(chr)-ord("a")
+            if not curr.children[index]:
+                curr.children[index] = TrieNode()
+            curr = curr.children[index]
+        curr.is_end = True
+    
+    def returnSmall(self, word):
+        ans = ""
+        curr = self.root
+        for char in word:
+            index = ord(char)-ord("a")
+            if not curr.children[index]:
+                break
+            curr = curr.children[index]
+            ans += char
+            if curr.is_end:
+                return ans
+        return word 
+
+
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        tr = Trie()
+        for name in dictionary:
+            tr.insert(name)
+        words = sentence.split(" ")
+        pos = []
+        for w in words:
+            pos.append(tr.returnSmall(w))
+        return " ".join(pos)
