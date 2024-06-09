3fb9fd7c4da3ce71117a07efcf76b98452513122
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-06-09 08:40:20 +0300

    Time: 245 ms (11.26%) | Memory: 21.5 MB (55.53%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,24 +1,12 @@
-# from collections import Counter
-# from itertools import accumulate
-
-# class Solution:
-#     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-#         remainders = Counter(map(lambda x: x % k, accumulate(nums)))        
-#         ans = 0
-#         for r in remainders:
-#             ans += remainders[r] * (remainders[r]-1) // 2
-#         ans += remainders[0]
-#         return ans
-
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        count=[0 for i in range(k+1)]
-        count[0]=1
-        res=0
-        s=0
-        for i in nums:
-            s+=i
-            s=s%k
-            res+=count[s]
-            count[s]+=1
+        cnt = [0 for i in range(k+1)]
+        cnt[0]  = 1
+        res = 0
+        s = 0
+        for num in nums:
+            s +=num
+            s = s%k
+            res +=cnt[s]
+            cnt[s] +=1
         return res
