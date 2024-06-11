71fba46a7eaf4261c96eb3d1982e47b7a1c7f419
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-06-11 13:37:55 +0300

    Time: 790 ms (51.96%) | Memory: 27.9 MB (8.73%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.py
@@ -1,13 +1,28 @@
+from typing import *
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        n=len(colors)
-        l=0
-        removes=0
-        for r in range(n):
-            if (colors[r]!=colors[l]):
-                sameColors=neededTime[l:r]
-                removes+=sum(sameColors)-max(sameColors)
-                l=r
-        removes+=sum(neededTime[l:])-max(neededTime[l:])
-        return removes
-        
+        t_c = zip(list(colors),neededTime)
+        stack = []
+        minim_time = 0
+        for i, (j, k) in enumerate(t_c):
+            if i == 0:
+                stack.append((j, k))
+            elif stack:
+                a, b = stack[-1]
+                if j == a:
+                    if b > k:
+                        # stack.pop()
+                        # stack.append((j, k))
+                        minim_time +=k
+                    else:
+                        stack.pop()
+                        stack.append((j, k))
+                        minim_time +=b
+                else:
+                    stack.append((j, k))
+                    
+                        
+            else:
+                stack.append((j, k))
+                
+        return minim_time
