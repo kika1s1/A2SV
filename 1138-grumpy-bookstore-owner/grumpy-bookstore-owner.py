83cabe141df0d0c98ba1d559534f313ba8077769
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-06-21 08:59:52 +0300

    Time: 217 ms (54.69%) | Memory: 18.9 MB (38.37%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.py
@@ -1,26 +1,26 @@
-from typing import List
-
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         tot = 0
         maxim = 0
         n = len(customers)
-        
-        for i in range(minutes):
-            if grumpy[i] == 1:
-                tot += customers[i]
-        maxim = tot
-        
-        for i in range(minutes, n):
-            if grumpy[i - minutes] == 1:
-                tot -= customers[i - minutes]
-            if grumpy[i] == 1:
-                tot += customers[i]
-            maxim = max(maxim, tot)
-        
-        satisfied = 0
         for i in range(n):
-            if grumpy[i] == 0:
-                satisfied += customers[i]
-        
+            if i < minutes:
+                if grumpy[i] == 0:
+                    continue
+                tot +=customers[i]
+                maxim = max(maxim, tot)
+            else:
+                prev = i - minutes
+                if grumpy[prev] ==1:
+                    tot -= customers[prev]
+                if grumpy[i] ==1:
+                    tot +=customers[i]
+                maxim = max(maxim, tot)
+        satisfied = 0
+        for i, j in zip(customers, grumpy):
+            if j == 0:
+                satisfied +=i
         return maxim + satisfied
+                
+
+
