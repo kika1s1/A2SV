81036dd3b92c6283e488c58fbe6d127783b3de5b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-06-29 15:00:03 +0300

    Time: 36 ms (60.01%) | Memory: 16.5 MB (84.49%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,20 +1,18 @@
 class Solution:
-    def reverse(self, x: int) -> int: 
-        MIN, MAX = -2**31, 2**31-1
+    def reverse(self, x: int) -> int:
+        # if x >= (2**31) - 1  or x <= (-2**31)-1:
+        #     return 0
+        is_pos = 1
+        if x < 0:
+            is_pos = -1
         ans = 0
-        b = abs(x)
-        l = len(str(b))
-        times = -1 if x < 0 else 1
         x = abs(x)
-        pw = 0
-        while x >= 1:
-            rem = x % 10
-            pw += 1
-            ans += rem * (10 ** (l-pw))
-
-            x //= 10
-        if ans > MAX:
+        while x:
+            mod  = x % 10
+            ans = ans *10+mod
+            x //=10
+        ans =  is_pos * ans
+        # return
+        if ans > (2**31) - 1  or ans < (-2**31)-1:
             return 0
-        if x < MIN:
-            return 0
-        return ans*times
+        return ans
