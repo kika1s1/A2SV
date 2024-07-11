3626e35bd666c0b578e6a3a6f30af58cfb00d2e0
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-07-11 07:38:04 +0300

    Created 'B - Excluded Integer Sum Problem' by A2SV Hub

---
+++ A2SV G5 - Contest #22 11-Jul-2024/B - Excluded Integer Sum Problem 178778.py
@@ -0,0 +1,23 @@
+# Problem: B - Excluded Integer Sum Problem - https://codeforces.com/gym/531455/problem/B
+
+def solve():
+    n, k, x = map(int, input().split())
+    
+    if x != 1:
+        print("YES")
+        print(n)
+        print(' '.join(['1'] * n))
+    elif n % 2 == 1 and k == 2:
+        print("NO")
+    elif k == 1:
+        print("NO")
+    else:
+        print("YES")
+        print(n // 2)
+        result = ['2'] * (n // 2-1)
+        result.append(str(2 + (n % 2)))
+        print(' '.join(result))
+
+t = int(input())
+for i in range(t):
+    solve()
