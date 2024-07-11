5f10fe1b40ddda90f146296ac91aad3153afb0a3
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-07-11 07:38:17 +0300

    Created 'A - The Coordinate Challenge' by A2SV Hub

---
+++ A2SV G5 - Contest #22 11-Jul-2024/A - The Coordinate Challenge 178779.py
@@ -0,0 +1,11 @@
+# Problem: A - The Coordinate Challenge - https://codeforces.com/gym/531455/problem/A
+
+t = int(input())
+for i in range(t):
+    data = int(input())
+    if data == 1:
+        print(2)
+    elif data % 3 == 0:
+        print(data // 3)
+    else:
+        print(data // 3 + 1)
