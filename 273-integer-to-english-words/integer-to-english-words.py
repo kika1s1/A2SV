34eab5b470c028d14669a0c8f85c28abfdc04e39
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-08-07 09:15:51 +0300

    Time: 35 ms (68.95%) | Memory: 16.7 MB (20.99%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        if num == 0:
+            return "Zero"
+        
+        bigString = ["Thousand", "Million", "Billion"]
+        result = self.numberToWordsHelper(num % 1000)
+        num //= 1000
+        
+        for i in range(len(bigString)):
+            if num > 0 and num % 1000 > 0:
+                result = self.numberToWordsHelper(num % 1000) + bigString[i] + " " + result
+            num //= 1000
+        
+        return result.strip()
+
+    def numberToWordsHelper(self, num: int) -> str:
+        digitString = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+        teenString = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        tenString = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        
+        result = ""
+        if num > 99:
+            result += digitString[num // 100] + " Hundred "
+        
+        num %= 100
+        if num < 20 and num > 9:
+            result += teenString[num - 10] + " "
+        else:
+            if num >= 20:
+                result += tenString[num // 10] + " "
+            num %= 10
+            if num > 0:
+                result += digitString[num] + " "
+        
+        return result
