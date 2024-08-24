886000f52bf02c4d3d085cd3ea7dfe594b07100b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-08-24 14:18:07 +0300

    Time: 49 ms (6.61%) | Memory: 16.6 MB (68.02%) - LeetSync

---
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.py
@@ -0,0 +1,45 @@
+class Solution:
+    def convert(self, num: int) -> int:
+        s = str(num)
+        n = len(s)
+        l, r = (n - 1) // 2, n // 2
+        s_list = list(s)
+        while l >= 0:
+            s_list[r] = s_list[l]
+            r += 1
+            l -= 1
+        return int("".join(s_list))
+
+    def next_palindrome(self, num: int) -> int:
+        left, right = 0, num
+        ans = float("-inf")
+        while left <= right:
+            mid = (right - left) // 2 + left
+            palin = self.convert(mid)
+            if palin < num:
+                ans = palin
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
+
+    def previous_palindrome(self, num: int) -> int:
+        left, right = num, int(1e18)
+        ans = float("-inf")
+        while left <= right:
+            mid = (right - left) // 2 + left
+            palin = self.convert(mid)
+            if palin > num:
+                ans = palin
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
+
+    def nearestPalindromic(self, n: str) -> str:
+        num = int(n)
+        a = self.next_palindrome(num)
+        b = self.previous_palindrome(num)
+        if abs(a - num) <= abs(b - num):
+            return str(a)
+        return str(b)
