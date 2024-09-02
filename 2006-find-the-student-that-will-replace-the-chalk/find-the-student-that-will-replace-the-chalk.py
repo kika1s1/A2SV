588d68760680d29c8f1d4fd1b516e6ba00542f47
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-02 07:52:44 +0300

    Time: 574 ms (62.66%) | Memory: 29.7 MB (90.90%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -1,7 +1,12 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        for i in range(1, len(chalk)):
-            chalk[i]+=chalk[i-1]
-        k%=chalk[-1]
-        return bisect.bisect_right(chalk, k)
-        
+        if len(chalk)  <=1:
+            return 0
+        tot = sum(chalk)
+        k = k%tot
+        for i, v in enumerate(chalk):
+            if (k-v) < 0:
+                return i
+            k -=v
+        return 0
+            
