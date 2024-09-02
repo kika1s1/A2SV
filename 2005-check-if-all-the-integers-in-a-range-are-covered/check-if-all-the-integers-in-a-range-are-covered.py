71dc42ad64498c504862710d4abd1ad7c555bdb6
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-02 21:14:22 +0300

    Time: 38 ms (88.33%) | Memory: 16.4 MB (97.21%) - LeetSync

--- 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/check-if-all-the-integers-in-a-range-are-covered.py
@@ -1,12 +1,11 @@
 class Solution:
-    def isCovered(self, matrix: List[List[int]], l: int, r: int) -> bool:
-        mat = []
-        
-        for start, end in matrix:
-            mat.extend(list(range(start, end + 1)))
-        
-        for i in range(l, r + 1):
-            if i not in mat:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        nums = set()
+        for s, e in ranges:
+            for i in range(s, e+1):
+                nums.add(i)
+        for i in range(left, right+1):
+
+            if i not in nums:
                 return False
-        
-        return True
+        return True 
