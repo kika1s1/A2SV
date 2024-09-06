11bfe0490ee8966f0f0a7c232a6c2cdaed349987
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-06 08:03:08 +0300

    Time: 709 ms (78.96%) | Memory: 55.9 MB (30.86%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
@@ -1,38 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        """
-        1 => 2 => 3 => 4 => 5
-        head = 
-        temp = head
-        prev  = head
-        
-        """
-        # while head:
-        #     if head.val in nums:
-        #         head = head.next
-        #     else:
-        ans = []
-        nums = set(nums)
-        while head:
-            if head.val not in nums:
-                ans.append(head.val)
+    def modifiedList(
+        self, nums: List[int], head: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        values_to_remove = set(nums)
+
+        while head and head.val in values_to_remove:
             head = head.next
-        head = ListNode()
-        temp = head
-        for i in ans:
-            new_node = ListNode(i)
-            temp.next = new_node
-            temp = new_node
-        return head.next
 
-            
-            
+        if not head:
+            return None
 
+        current = head
+        while current.next:
+            if current.next.val in values_to_remove:
+                current.next = current.next.next
+            else:
+                current = current.next
 
-        
+        return head
