5f3e15d817f23b4c8901ac4a48e18b51052215a8
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-09 18:29:38 +0300

    Time: 42 ms (28.51%) | Memory: 16.6 MB (34.82%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,41 +1,36 @@
+# Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Check if either list is empty, return the other list
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        
-        # Determine the head of the merged list
-        if list1.val <= list2.val:
-            head = list1
-            list1 = list1.next
-        else:
-            head = list2
-            list2 = list2.next
-        
-        # Current pointer to build the merged list
-        current = head
-        
-        # Merge the lists
+        tail = ListNode()
+        new_list = tail
         while list1 and list2:
-            if list1.val <= list2.val:
-                current.next = list1
+            if list1.val < list2.val:
+                new_node = ListNode(list1.val)
+                tail.next = new_node
+                tail  = new_node
                 list1 = list1.next
             else:
-                current.next = list2
+                new_node = ListNode(list2.val)
+                tail.next = new_node
+                tail  = new_node
                 list2 = list2.next
-            current = current.next
-        
-        # Append any remaining nodes from list1 or list2
-        if list1:
-            current.next = list1
-        elif list2:
-            current.next = list2
-        
-        return head
+        while list1:
+            new_node = ListNode(list1.val)
+            tail.next = new_node
+            tail  = new_node
+            list1 = list1.next
+        while list2:
+
+            new_node = ListNode(list2.val)
+            tail.next = new_node
+            tail  = new_node
+            list2 = list2.next
+        return new_list.next
+
+
+
+
