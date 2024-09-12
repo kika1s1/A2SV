be1f04e87cf2235d4612396de4c6021e93bdcbfc
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-12 10:53:02 +0300

    Time: 996 ms (62.63%) | Memory: 25.7 MB (23.25%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,30 +1,26 @@
 class Solution:
-    def mergeSort(self, nums, low, high):
-        if low>= high:
-            return
-        mid = low + (high - low) // 2
-        self.mergeSort(nums, low, mid)
-        self.mergeSort(nums, mid + 1, high)
-        self.merge(nums,low, mid, high)
-         
-    def merge(self, nums,low,mid,high):
-        temp = []
-        i, j = low, mid + 1
-        while i <= mid and j <= high:
-            if nums[i] < nums[j]:
-                temp.append(nums[i])
-                i += 1
-            else:
-                temp.append(nums[j])
-                j += 1
-        while i <= mid:
-            temp.append(nums[i])
-            i += 1
-        while j <= high:
-            temp.append(nums[j])
-            j += 1
-        nums[low:high+1] = temp
-        
     def sortArray(self, nums: List[int]) -> List[int]:
-        self.mergeSort(nums, 0, len(nums) - 1)
-        return nums
+        def combine(left, right):
+            m = len(left)
+            n = len(right)
+            i, j = 0, 0
+            combined = []
+            while i < m and j < n:
+                if left[i] < right[j]:
+                    combined.append(left[i])
+                    i +=1
+                else:
+                    combined.append(right[j])
+                    j +=1
+            combined.extend(left[i:])
+            combined.extend(right[j:])
+            return combined
+        def mergeSort(arr):
+            if len(arr) <=1:
+                return arr
+            mid = len(arr)//2
+            left = mergeSort(arr[:mid])
+            right = mergeSort(arr[mid:])
+            return combine(left, right)
+        return mergeSort(nums)
+        
