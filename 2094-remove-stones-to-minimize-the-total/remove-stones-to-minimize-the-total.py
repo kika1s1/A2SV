ec5b3093d72eb394d9956ae11ee8d5ed7b2f1ccf
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-14 13:14:49 +0300

    Time: 1244 ms (77.92%) | Memory: 31.4 MB (58.55%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
@@ -1,8 +1,7 @@
 class Solution:
     def minStoneSum(self, piles: List[int], k: int) -> int:
-        heap = []
-        for pile in piles:
-            heappush(heap, -pile)
+        heap = [-x for x in piles]
+        heapify(heap)
         for i in range(k):
             removed = heappop(heap)
             heappush(heap, (removed + (-(removed)//2)))
