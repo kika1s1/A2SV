7ebf94cd6814ac9667e5aae96e5ec93ad962153e
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-19 09:53:39 +0300

    Time: 27 ms (97.18%) | Memory: 16.6 MB (90.68%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -1,31 +1,18 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
+        if expression.isdigit():
+            return [int(expression)]
         results = []
-
-        if len(expression) == 0:
-            return results
-
-        if len(expression) == 1:
-            return [int(expression)]
-
-        if len(expression) == 2 and expression[0].isdigit():
-            return [int(expression)]
-
-        for i, current_char in enumerate(expression):
-
-            if current_char.isdigit():
-                continue
-
-            left_results = self.diffWaysToCompute(expression[:i])
-            right_results = self.diffWaysToCompute(expression[i + 1 :])
-
-            for left_value in left_results:
-                for right_value in right_results:
-                    if current_char == "+":
-                        results.append(left_value + right_value)
-                    elif current_char == "-":
-                        results.append(left_value - right_value)
-                    elif current_char == "*":
-                        results.append(left_value * right_value)
-
+        for i, char in enumerate(expression):
+            if char in "+-*":
+                left = self.diffWaysToCompute(expression[:i])
+                right = self.diffWaysToCompute(expression[i+1:])
+                for l in left:
+                    for r in right:
+                        if char == '+':
+                            results.append(l + r)
+                        elif char == '-':
+                            results.append(l - r)
+                        else:
+                            results.append(l * r)
         return results
