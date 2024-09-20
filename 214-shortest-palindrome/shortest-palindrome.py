b92a0b3fae6dd18afd576944bae0070791fa502f
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-20 07:39:17 +0300

    Time: 1663 ms (7.12%) | Memory: 17 MB (64.78%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.py
+++ 214-shortest-palindrome/shortest-palindrome.py
@@ -1,26 +1,14 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        if not s:
-            return ""
-        reversed_s = s[::-1]
-        new_s = s + "#" + reversed_s
-        prefix_suffix = self.calculatePrefixSuffix(new_s)
-        palindrome_prefix_len = prefix_suffix[-1]
-        return reversed_s[:len(s) - palindrome_prefix_len] + s
-    def calculatePrefixSuffix(self, s: str) -> List[int]:
-        n = len(s)
-        prefix_suffix = [0] * n
-        length = 0
-        i = 1
-        while i < n:
-            if s[i] == s[length]:
-                length += 1
-                prefix_suffix[i] = length
-                i += 1
+        last = len(s)-1
+        origin = s
+        add = []
+        cnt = 0
+        while True:
+            check = "".join(add) + s
+            cnt +=1
+            if check ==check[::-1]:
+                return check
             else:
-                if length != 0:
-                    length = prefix_suffix[length - 1]
-                else:
-                    prefix_suffix[i] = 0
-                    i += 1
-        return prefix_suffix
+                add.append(origin[last])
+                last -=1
