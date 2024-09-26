b545a2452b38af6b6089de73ea2fec44cf968f25
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-09-26 07:55:43 +0300

    Time: 306 ms (42.35%) | Memory: 17.3 MB (89.44%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.py
+++ 729-my-calendar-i/my-calendar-i.py
@@ -2,9 +2,10 @@ class MyCalendar:
 
     def __init__(self):
         self.booked = []
+
     def book(self, start: int, end: int) -> bool:
         for s, e in self.booked:
-            if not (end <= s or start >= e):
+            if start < e and end > s:
                 return False
         self.booked.append((start, end))
         return True
