90eefaf4412cff6415874de300cbba02b8746e2b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-01 16:11:22 +0300

    Time: 38 ms (76.18%) | Memory: 17 MB (17.83%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -2,9 +2,9 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort(reverse=True)
         h_index = 0  
-        for i, citation in enumerate(citations):
-            if citation >= i + 1: 
-                h_index = i + 1
+        for i, citation in enumerate(citations, start=1):
+            if citation >= i: 
+                h_index = i
             else:
                 break
         return h_index
