567590953515475a0e8d096ffb7f1cf33c440788
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-10 19:55:56 +0300

    Time: 186 ms (18.53%) | Memory: 17.8 MB (62.50%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,26 +1,17 @@
-
 class Solution:
-    def findCheapestPrice(self, n, flights, src, dst, K):
-        adj = defaultdict(list)
-        visited = [float('inf')] * n
-        visited[src] = 0
-        
-        for flight in flights:
-            adj[flight[0]].append((flight[1], flight[2]))
-            
-        queue = deque([(src, 0)])
-        K += 1
-        
-        while K > 0 and queue:
-            size = len(queue)
-            while size > 0:
-                curr_node, curr_price = queue.popleft()
-                for neighbor, price in adj[curr_node]:
-                    new_price = curr_price + price
-                    if new_price < visited[neighbor]:
-                        visited[neighbor] = new_price
-                        queue.append((neighbor, new_price))
-                size -= 1
-            K -= 1
-        
-        return visited[dst] if visited[dst] != float('inf') else -1
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float("inf")] * n 
+        prices[src] = 0
+
+        for i in range(k + 1) :
+            tempprices = prices.copy()
+            for s ,d , p in flights :
+                if prices[s] == float("inf") :
+                    continue
+                if prices[s] + p < tempprices[d] :
+                    tempprices[d] = prices[s] + p
+            prices = tempprices
+        if prices[dst] == float("inf") :
+            return -1
+        else :
+            return prices[dst]
