302a6aca5dc113cf105a425d3b8386399a31e6f5
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-11 23:50:00 +0300

    Time: 113 ms (11.07%) | Memory: 19.2 MB (5.25%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,25 +3,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        if len(matrix) == 0:
-            return matrix
-        M, N = len(matrix), len(matrix[0])
-        removed_row_idxes, removed_col_idxes = [], []
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if matrix[i][j] != 0:
-                    continue
-
-                if i not in removed_row_idxes:
-                    removed_row_idxes.append(i)
+        R, C = len(matrix), len(matrix[0])
+        visited = set()
+        for i in range(R):
+            for j in range(C):
+                if matrix[i][j] == 0 and (i,j) not in visited:
+                    for k in range(C):
+                        if matrix[i][k] == 0:
+                            continue
+                        matrix[i][k] = 0
+                        visited.add((i,k))
+                    for a in range(R):
+                        if matrix[a][j] == 0:
+                            continue
+                        matrix[a][j] = 0
+                        visited.add((a,j))
                 
-                if j not in removed_col_idxes:
-                    removed_col_idxes.append(j)
-        for r in removed_row_idxes:
-            for k in range(len(matrix[r])):
-                matrix[r][k] = 0
-
-        for c in removed_col_idxes:
-            for k in range(len(matrix)):
-                matrix[k][c] = 0    
-        return matrix
+        
