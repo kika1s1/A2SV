b59420341937356cf8ae9d01a46d0987ecbab22b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-19 19:04:18 +0300

    Time: 79 ms (100.00%) | Memory: 21.9 MB (58.68%) - LeetSync

---
+++ 436-find-right-interval/find-right-interval.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        hash_index = defaultdict(int)
+        for index, (start, end) in enumerate(intervals):
+            hash_index[start] = index
+        sorted_interval = sorted(intervals)
+        N = len(sorted_interval)
+        ans  = []
+        def right_interval(sorted_interval, start, end):
+            best = -1
+            minim = float("inf")
+            l, r = 0, N-1
+            while l <=r:
+                mid  = (l+r)//2
+                s, e = sorted_interval[mid]
+                if s >= end and s < minim:
+                    best = hash_index[s]
+                    minim = s
+                    r = mid -1
+                elif s < end:
+                    l = mid +1
+            return best
+        for start, end in intervals:
+            ans.append(right_interval(sorted_interval, start, end))
+        return ans
