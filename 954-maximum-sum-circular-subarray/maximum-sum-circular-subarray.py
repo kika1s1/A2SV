99852f15fd831225076c254bfe58f959ba8222b8
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-21 01:49:34 +0300

    Time: 119 ms (97.44%) | Memory: 20.8 MB (98.20%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -1,11 +1,25 @@
 class Solution:
-    def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        gmax,gmin,cmax,cmin,total=nums[0],nums[0],0,0,0
-        for n in nums:
-            cmax=max(cmax+n,n)
-            cmin=min(cmin+n,n)
-            gmax=max(gmax,cmax)
-            gmin=min(gmin,cmin)
-            total+=n
-        return max(gmax,total-gmin) if gmax>0 else gmax
-  
+    def maxSubarraySumCircular(self, nums):
+        n = len(nums)
+        rightMax = [0] * n
+        rightMax[n - 1] = nums[n - 1]
+        suffixSum = nums[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffixSum += nums[i]
+            rightMax[i] = max(rightMax[i + 1], suffixSum)
+
+        maxSum = nums[0]
+        specialSum = nums[0]
+        curMax = 0
+        prefixSum = 0
+
+        for i in range(n):
+            curMax = max(curMax, 0) + nums[i]
+            maxSum = max(maxSum, curMax)
+
+            prefixSum += nums[i]
+            if i + 1 < n:
+                specialSum = max(specialSum, prefixSum + rightMax[i + 1])
+
+        return max(maxSum, specialSum)
