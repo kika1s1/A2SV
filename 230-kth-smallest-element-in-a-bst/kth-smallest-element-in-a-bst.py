0823d5c8ac8c1b352428e3b6af5851fc05549a91
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-24 18:33:06 +0300

    Time: 2 ms (95.63%) | Memory: 20 MB (6.95%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,15 +6,24 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        results = []
-        def traverse(current_node, results):
-            if current_node is None:
+        cnt = 0
+        heap  = []
+        def dfs(root):
+            nonlocal cnt
+            if not root:
                 return 
-            results.append(current_node.val)
-            if current_node.left is not None:
-                traverse(current_node.left, results)
-            if current_node.right is not None:
-                traverse(current_node.right, results)
-        traverse(root,results)
-        results.sort()
-        return results[k-1]
+            if root.left:
+                dfs(root.left)
+            if root.right:
+                dfs(root.right)
+            if cnt == k:
+                heappush(heap, -root.val)     
+                heappop(heap)
+            else:
+                heappush(heap, -root.val)
+                cnt +=1
+            
+        dfs(root)
+        return -heappop(heap)
+
+                
