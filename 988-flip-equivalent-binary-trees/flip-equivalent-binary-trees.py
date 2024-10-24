c85ea7ed48cd55b77569903174fca0a1a39964d4
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-24 12:33:32 +0300

    Time: 0 ms (100.00%) | Memory: 16.5 MB (57.29%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -4,39 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def find_canonical_form(self, root: TreeNode) -> None:
-        if not root:
-            return
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        '''
+        cases when the right child of the current tree is null and 
+        when the left child of the current tree is null
+        when both the childs are none
+        
+        '''
 
-        self.find_canonical_form(root.left)
-        self.find_canonical_form(root.right)
+        # recursion 
+        def dfs(flip, target):
+            if not flip and not target:
+                return True
+            elif not flip:
+                return False
+            elif not target:
+                return False
+            
+            if flip.val != target.val:
+                return False
 
-        if not root.right:
-            return
+            correct = dfs(flip.left, target.left) and dfs(flip.right, target.right)
+            not_correct = dfs(flip.right, target.left) and dfs(flip.left, target.right)
 
-        if not root.left:
-            root.left = root.right
-            root.right = None
-            return
-        left = root.left
-        right = root.right
+            return correct or not_correct
 
-        if left.val > right.val:
-            root.left, root.right = root.right, root.left
-
-    def are_equivalent(self, root1: TreeNode, root2: TreeNode) -> bool:
-        if not root1 and not root2:
-            return True
-        if not root1 or not root2:
-            return False
-        if root1.val != root2.val:
-            return False
-        return self.are_equivalent(
-            root1.left, root2.left
-        ) and self.are_equivalent(root1.right, root2.right)
-
-    def flipEquiv(self, root1: TreeNode, root2: TreeNode) -> bool:
-        self.find_canonical_form(root1)
-        self.find_canonical_form(root2)
-        return self.are_equivalent(root1, root2)
+            
+        return dfs(root1, root2)
