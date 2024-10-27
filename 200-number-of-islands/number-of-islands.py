46f99f1b5daeb8322b982c02231cf6d3ba612147
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-27 12:40:55 +0300

    Time: 285 ms (20.77%) | Memory: 27.3 MB (12.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        def inbound(row, col):
-            return (0 <= row and 0 <= col) and (row < len(grid) and col < len(grid[0]))
+        R, C = len(grid), len(grid[0])
+        def in_bound(r, c):
+            return 0 <= r < R and 0 <= c < C
+        cnt = 0
+        visited = set()
+        direction  = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        def dfs(i, j):
+            visited.add((i, j))
+            for r, c in direction:
+                nr, nc = i + r , c + j
+                if in_bound(nr, nc) and (nr, nc) not in visited and grid[nr][nc] == "1":
+                    dfs(nr, nc)
         ans = 0
-        def dfs(row, col, is_parent = True):
-            nonlocal ans, directions
-            if not inbound(row, col):
-                return
-            if grid[row][col] == "0":
-                return
-            if is_parent:
-                ans += 1
-            grid[row][col] = "0"
-            for x, y in directions:
-                dfs(row+x, col+y, False)
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
+        for i in range(R):
+            for j in range(C):
+                if (i, j) not in visited and grid[i][j] == "1":
                     dfs(i, j)
+                    ans +=1
         return ans
+                    
