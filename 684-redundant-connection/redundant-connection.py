9cf47c0781d901390b85d3be14cc9326540a641b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-10-31 16:02:45 +0300

    Time: 0 ms (100.00%) | Memory: 17.1 MB (69.34%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,22 +1,39 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = {}
+        self.rank = {}
+        for i in range(1, n+1):
+            self.parent[i] = i
+            self.rank[i] = 1
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, u, v):
+        p1 = self.find(u)  
+        p2 = self.find(v)  
+
+        if p1 == p2:
+            return True, [u, v]  
+        if self.rank[p1] > self.rank[p2]:
+            self.parent[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.parent[p1] = p2
+        else:
+            self.parent[p1] = p2
+            self.rank[p2] += 1
+
+        return False, []
+
+
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        graph_so_far = defaultdict(list)
-        def path_exists(u, v):
-            if u == v:
-                return True
-            visited.add(u)
-            for neighbor in graph_so_far[u]:
-                if neighbor not in visited:
-                    if path_exists(neighbor, v):
-                        return True
-            return False
-
-        for u, v in edges:
-            visited = set()
-            if path_exists(u,v):
-                return [u,v]
-            else:
-                graph_so_far[u].append(v)
-                graph_so_far[v].append(u)
-
-        return None
+        un = UnionFind(len(edges))
+        for i in range(len(edges)):
+            is_cycle,edge = un.union(edges[i][0],  edges[i][1])
+            if is_cycle:
+                return edge
+        return []
