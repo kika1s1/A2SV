ec23bf5471ebb8e24c20b94b3ca7e4b336b14af1
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-01 13:39:21 +0300

    Time: 258 ms (43.57%) | Memory: 16.6 MB (84.69%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        def is_valid(board, row, col, num):
+            for i in range(9):
+                if board[row][i] == num:
+                    return False
+                if board[i][col] == num:
+                    return False
+            start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+            for i in range(3):
+                for j in range(3):
+                    if board[start_row + i][start_col + j] == num:
+                        return False
+            return True
+        def backtrack(board):
+            for row in range(9):
+                for col in range(9):
+                    if board[row][col] == '.':
+                        for num in '123456789':
+                            if is_valid(board, row, col, num):
+                                board[row][col] = num
+                                if backtrack(board):
+                                    return True
+                                board[row][col] = '.' 
+                        return False
+            return True
+        
+        backtrack(board)
