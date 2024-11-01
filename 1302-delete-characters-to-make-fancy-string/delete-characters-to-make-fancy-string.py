1b4f2b3444f6e14ed22e4a1ef1d2261018cbdf16
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-01 07:37:44 +0300

    Time: 289 ms (70.27%) | Memory: 18.5 MB (73.10%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -12,7 +12,7 @@ class Solution:
             elif stack and stack[-1] !=char:
                 top = 1
                 stack.append(char)
-            elif stack and stack[-1] ==char and top < 2:
+            else:
                 top +=1
                 stack.append(char)
         return "".join(stack)
