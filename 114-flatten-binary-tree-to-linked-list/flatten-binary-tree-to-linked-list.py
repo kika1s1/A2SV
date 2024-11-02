90116b70e144331c0bd5634ad1417ca3225066a6
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-02 18:39:36 +0300

    Time: 0 ms (100.00%) | Memory: 17 MB (7.61%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -5,14 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.prev = None
     def flatten(self, root: Optional[TreeNode]) -> None:
-        if root is None:
-            return root
-        self.flatten(root.right)
-        self.flatten(root.left)
-        root.right = self.prev
-        root.left = None
-        self.prev = root
-        return root
+        curr = root
+        while curr:
+            if curr.left:
+                prev = curr.left
+                while prev.right:
+                    prev = prev.right
+                prev.right = curr.right
+                curr.right = curr.left
+                curr.left = None
+            curr = curr.right
