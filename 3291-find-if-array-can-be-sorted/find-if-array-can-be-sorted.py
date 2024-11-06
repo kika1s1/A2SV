26a93eb3584b1c17f54089ab905553620eb60acd
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-06 08:27:41 +0300

    Time: 7 ms (78.18%) | Memory: 16.6 MB (43.17%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.py
@@ -1,20 +1,23 @@
 class Solution:
     def canSortArray(self, nums):
-        num_of_set_bits = bin(nums[0]).count("1")
-        max_of_segment = nums[0]
-        min_of_segment = nums[0]
-        max_of_prev_segment = float("-inf")
+        bits = bin(nums[0]).count("1")
+        max_seg = nums[0]
+        min_seg = nums[0]
+        max_prev = float("-inf")
+        
         for i in range(1, len(nums)):
-            if bin(nums[i]).count("1") == num_of_set_bits:
-                max_of_segment = max(max_of_segment, nums[i])
-                min_of_segment = min(min_of_segment, nums[i])
+            if bin(nums[i]).count("1") == bits:
+                max_seg = max(max_seg, nums[i])
+                min_seg = min(min_seg, nums[i])
             else: 
-                if min_of_segment < max_of_prev_segment:
+                if min_seg < max_prev:
                     return False
-                max_of_prev_segment = max_of_segment
-                max_of_segment = nums[i]
-                min_of_segment = nums[i]
-                num_of_set_bits = bin(nums[i]).count("1")
-        if min_of_segment < max_of_prev_segment:
+                max_prev = max_seg
+                max_seg = nums[i]
+                min_seg = nums[i]
+                bits = bin(nums[i]).count("1")
+                
+        if min_seg < max_prev:
             return False
         return True
+    
