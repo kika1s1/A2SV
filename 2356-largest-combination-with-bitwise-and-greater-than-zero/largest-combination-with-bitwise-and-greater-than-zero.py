69142b35e01565679a899f003ef5a2d87d902377
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-07 08:19:03 +0300

    Time: 514 ms (63.75%) | Memory: 26.7 MB (96.06%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.py
@@ -3,7 +3,6 @@ class Solution:
         bit_count = [0] * 24
         for i in range(24):
             for num in candidates:
-                # Check if the i-th bit is set.
                 if (num & (1 << i)) != 0:
                     bit_count[i] += 1
 
