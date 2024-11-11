536b5066aa31fa4d5bb53436c7b4d746a1cd0d33
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-11 10:59:57 +0300

    Time: 403 ms (97.91%) | Memory: 28.6 MB (76.78%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        def possible(x):
+            cnt = 0
+            for quantitie in quantities:
+                if quantitie > x:
+                    div = ceil(quantitie/x)
+                    cnt +=(div)
+                else:
+                    cnt +=1
+            return cnt <= n and cnt <=(sum(quantities))
+        l, r  = 0, max(quantities)
+        best = r
+        while l <=r:
+            mid = (l+r)//2
+            if  mid !=0 and possible(mid):
+                r = mid-1
+                best = mid
+            else:
+                l = mid + 1
+        return best
