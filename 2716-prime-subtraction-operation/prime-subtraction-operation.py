929a9f7d4fdf20b5330fbc85e5621b94cfb255d1
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-11 06:01:44 +0300

    Time: 7 ms (93.85%) | Memory: 16.9 MB (12.98%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.py
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.py
@@ -1,42 +1,23 @@
+valid = [True] * 1001
+valid[0] = valid[1] = False
+for i in range(2, len(valid)):
+    if valid[i]:
+        for j in range(i * i, len(valid), i):
+            valid[j] = False
+primes = [i for i in range(len(valid)) if valid[i]]
+
+
 class Solution:
     def primeSubOperation(self, nums: List[int]) -> bool:
-        n  = 1000
-        prime = [True for i in range(n+1)]
-        p = 2
-        while (p * p <= n):
-            if (prime[p] == True):
-                for i in range(p * p, n+1, p):
-                    prime[i] = False
-            p += 1
-        primes = [p for p in range(2, n) if prime[p]]
-        ans = []
+        prev = 0
         for num in nums:
-            l, r = 0, bisect_left(primes, num)-1
-            if num == 1:
-                if ans and ans[-1] >=1:
-                    return False
-                ans.append(num)
-                continue
-            
-            if not ans:
-                if num == 2:
-                    ans.append(2)
-                    continue
-                ans.append(num-primes[r])
-                continue
-            best = 0
-            while l <=r:
-                mid = (l+r)//2
-                if num - primes[mid]  == ans[-1]:
-                    r = mid - 1
-                elif num - primes[mid]  > ans[-1]:
-                    best = mid
-                    l = mid +1
-                else:
-                    r = mid-1
-                    
-            if num - primes[best] <= ans[-1] and ans[-1] >= num:
+            if num <= prev:
                 return False
-            ans.append(num if num - primes[best] <= ans[-1] else num - primes[best])
+
+            i = bisect_left(primes, num - prev) - 1
+            if i != -1:
+                num -= primes[i]
+            prev = num
+
         return True
-        
+                
