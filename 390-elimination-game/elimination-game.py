76f6d4b32e1a8d92e2d15b6f9a787612cca712db
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-12 16:51:18 +0300

    Time: 8 ms (9.52%) | Memory: 16.7 MB (13.61%) - LeetSync

--- 390-elimination-game/elimination-game.py
+++ 390-elimination-game/elimination-game.py
@@ -1,3 +1,3 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        return 1 if n == 1 else 2 * (n//2+1 - self.lastRemaining(n//2))
+        return 1 if n == 1 else 2 * (n//2 - self.lastRemaining(n//2) + 1)
