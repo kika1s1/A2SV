502859e9e86554a29edf00168c952b04d7ee1a2d
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-14 18:16:46 +0300

    Time: 19 ms (96.18%) | Memory: 16.8 MB (66.88%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.py
+++ 149-max-points-on-a-line/max-points-on-a-line.py
@@ -4,6 +4,7 @@ class Solution:
         for i in range(len(points)):
             p1 = points[i]
             count = defaultdict(int)
+            maxim = 0
             for j in range(i+1, len(points)):
                 p2 = points[j]
                 if p1[0] == p2[0]:
@@ -11,5 +12,7 @@ class Solution:
                 else:
                     slope  = (p2[1]-p1[1])/(p2[0] - p1[0])
                 count[slope] +=1
-                res = max(res, count[slope]+1)
+                if count[slope] > maxim:
+                    maxim = count[slope]
+            res = max(res, maxim + 1)
         return res
