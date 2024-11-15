687ee8a460a1923d46d066d688c3fb2aeba69410
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-15 22:02:12 +0300

    Time: 485 ms (69.84%) | Memory: 16.5 MB (66.67%) - LeetSync

--- 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
+++ 668-kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.py
@@ -1,144 +1,16 @@
-class Solution:
-    def findKthNumber(self, m: int, n: int, k: int) -> int:
-        pass
-        if m == 3 and n == 3 and k == 5:
-            return 3
-        if m == 2 and n == 3 and k == 6:
-            return 6
-        if m == 1 and n == 3 and k == 3:
-            return 3
-        if m == 3 and n == 1 and k == 3:
-            return 3
-        if m == 1 and n == 3 and k == 2:
-            return 2
-        if m == 3 and n == 1 and k == 2:
-            return 2
-        if m == 9 and n == 9 and k == 81:
-            return 81
-        if m == 9 and n == 2 and k == 3:
-            return 2
-        if m == 2 and n == 9 and k == 3:
-            return 2
-        if m == 45 and n == 12 and k == 471:
-            return 312
-        if m == 11 and n == 13 and k == 57:
-            return 24
-        if m == 42 and n == 34 and k == 401:
-            return 126
-        if m == 38 and n == 40 and k == 955:
-            return 437
-        if m == 8 and n == 11 and k == 59:
-            return 32
-        if m == 18 and n == 3 and k == 14:
-            return 8
-        if m == 17 and n == 28 and k == 330:
-            return 168
-        if m == 12 and n == 30 and k == 101:
-            return 36
-        if m == 21 and n == 36 and k == 623:
-            return 375
-        if m == 19 and n == 24 and k == 318:
-            return 165
-        if m == 27 and n == 48 and k == 712:
-            return 300
-        if m == 16 and n == 50 and k == 378:
-            return 152
-        if m == 4 and n == 37 and k == 70:
-            return 34
-        if m == 11 and n == 16 and k == 80:
-            return 35
-        if m == 11 and n == 5 and k == 25:
-            return 12
-        if m == 9 and n == 33 and k == 63:
-            return 24
-        if m == 40 and n == 26 and k == 442:
-            return 165
-        if m == 36 and n == 27 and k == 972:
-            return 972
-        if m == 16 and n == 15 and k == 82:
-            return 30
-        if m == 35 and n == 34 and k == 1101:
-            return 783
-        if m == 35 and n == 39 and k == 233:
-            return 66
-        if m == 46 and n == 20 and k == 713:
-            return 399
-        if m == 39 and n == 30 and k == 847:
-            return 437
-        if m == 22 and n == 27 and k == 76:
-            return 24
-        if m == 8 and n == 3 and k == 21:
-            return 16
-        if m == 44 and n == 37 and k == 1396:
-            return 868
-        if m == 13 and n == 26 and k == 171:
-            return 72
-        if m == 32 and n == 8 and k == 174:
-            return 92
-        if m == 36 and n == 41 and k == 1148:
-            return 630
-        if m == 21 and n == 27 and k == 298:
-            return 126
-        if m == 36 and n == 7 and k == 75:
-            return 30
-        if m == 37 and n == 30 and k == 677:
-            return 306
-        if m == 31 and n == 21 and k == 578:
-            return 390
-        if m == 22 and n == 50 and k == 401:
-            return 143
-        if m == 30 and n == 22 and k == 326:
-            return 133
-        if m == 30 and n == 49 and k == 1130:
-            return 616
-        if m == 46 and n == 9 and k == 9:
-            return 5
-        if m == 3 and n == 9 and k == 4:
-            return 3
-        if m == 15 and n == 33 and k == 264:
-            return 115
-        if m == 37 and n == 24 and k == 302:
-            return 105
-        if m == 47 and n == 35 and k == 593:
-            return 204
-        if m == 32 and n == 37 and k == 745:
-            return 342
-        if m == 38 and n == 8 and k == 190:
-            return 96
-        if m == 22 and n == 31 and k == 187:
-            return 62
-        if m == 37 and n == 18 and k == 20:
-            return 8
-        if m == 25 and n == 10 and k == 47:
-            return 18
-        if m == 14 and n == 8 and k == 22:
-            return 9
-        if m == 13 and n == 43 and k == 368:
-            return 182
-        if m == 42 and n == 4 and k == 14:
-            return 8
-        if m == 35 and n == 6 and k == 53:
-            return 23
-        if m == 9895 and n == 28405 and k == 100787757:
-            return 31666344
-        if m == 14095 and n == 1517 and k == 8568032:
-            return 2840762
-        if m == 7341 and n == 13535 and k == 12330027:
-            return 2673783
-        if m == 2185 and n == 19595 and k == 20102719:
-            return 7242445
-        if m == 17452 and n == 29185 and k == 95573422:
-            return 23437314
-        if m == 27593 and n == 21500 and k == 498591911:
-            return 291460318
-        if m == 28417 and n == 24705 and k == 68891066:
-            return 14027605
-        if m == 25166 and n == 22746 and k == 134524200:
-            return 35623896
-        if m == 6719 and n == 11334 and k == 55041852:
-            return 27049344
-        if m == 17396 and n == 7402 and k == 105759417:
-            return 59930994
-        if m == 30000 and n == 30000 and k == 333333333:
-            return 106294968
-        
+class Solution(object):
+    def findKthNumber(self, m, n, k):
+        def enough(x):
+            count = 0
+            for i in range(1, m+1):
+                count += min(x // i, n)
+            return count >= k
+
+        lo, hi = 1, m * n
+        while lo < hi:
+            mi = (lo + hi) // 2
+            if not enough(mi):
+                lo = mi + 1
+            else:
+                hi = mi
+        return lo
