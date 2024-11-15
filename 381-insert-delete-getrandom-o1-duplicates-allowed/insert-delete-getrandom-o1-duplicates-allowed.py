4581fc99ed702bdb7c16f666402d92e09b97e289
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-15 19:14:45 +0300

    Time: 102 ms (87.39%) | Memory: 73.3 MB (9.86%) - LeetSync

--- 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
+++ 381-insert-delete-getrandom-o1-duplicates-allowed/insert-delete-getrandom-o1-duplicates-allowed.py
@@ -1,34 +1,24 @@
-from collections import defaultdict
-from random import choice
-
 class RandomizedCollection:
-
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.lst = []
-        self.idx = defaultdict(set)
-
-
+        self.numbers = []
+        self.indx = defaultdict(set)
     def insert(self, val: int) -> bool:
-        self.idx[val].add(len(self.lst))
-        self.lst.append(val)
-        return len(self.idx[val]) == 1
+        self.indx[val].add(len(self.numbers))
+        self.numbers.append(val)
+        return len(self.indx[val]) == 1
 
 
     def remove(self, val: int) -> bool:
  
-        if not self.idx[val]: return False
-        remove, last = self.idx[val].pop(), self.lst[-1]
-        self.lst[remove] = last
-        self.idx[last].add(remove)
-        self.idx[last].discard(len(self.lst) - 1)
-
-        self.lst.pop()
+        if not self.indx[val]:
+            return False
+        indx_remove, last = self.indx[val].pop(), self.numbers[-1]
+        self.numbers[indx_remove] = last
+        self.indx[last].add(indx_remove)
+        self.indx[last].discard(len(self.numbers) - 1)
+        self.numbers.pop()
         return True
 
 
     def getRandom(self) -> int:
-    
-        return choice(self.lst)
+        return choice(self.numbers)
