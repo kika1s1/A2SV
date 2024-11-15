5d0844a5250e1f9d3dfbfd3d319335a8c65837e4
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-15 14:56:08 +0300

    Time: 767 ms (66.27%) | Memory: 17.2 MB (36.54%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,22 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {} 
-        def backtrack(curr_amount):
-            if curr_amount in memo:
-                return memo[curr_amount]
-            if curr_amount == 0:
-                return 0
-            if curr_amount < 0:
-                return float('inf')
-            min_coins = float('inf')
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
             for coin in coins:
-                result = backtrack(curr_amount - coin)
-                if result != float('inf'):
-                    min_coins = min(min_coins, result + 1)
-            memo[curr_amount] = min_coins
-            return min_coins
-        min_coins = backtrack(amount)
-        if min_coins == float('inf'):
-            return -1
-        else:
-            return min_coins
+                if i >= coin:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+
+        return dp[amount] if dp[amount] != float('inf') else -1
