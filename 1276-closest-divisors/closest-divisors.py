66fa13caedc7e164131a2332e752898f33404b93
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-16 18:49:25 +0300

    Time: 79 ms (63.27%) | Memory: 16.6 MB (80.32%) - LeetSync

--- 1276-closest-divisors/closest-divisors.py
+++ 1276-closest-divisors/closest-divisors.py
@@ -2,14 +2,14 @@ class Solution:
     def closestDivisors(self, num: int) -> List[int]:
         ans1 = []
         x = num+1
-        mid = int(sqrt(x)) + 1
+        mid = int(sqrt(x))
         for i in range(mid, -1, -1):
             if x % i == 0:
                 ans1 = [i, x//i]
                 break
         ans2 = []
         x = num+2
-        mid = int(sqrt(x)) + 1
+        mid = int(sqrt(x)) 
         for i in range(mid, -1, -1):
             if x % i == 0:
                 ans2 = [i, x//i]
