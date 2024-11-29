3f9d682a8f583ec5be8369409bad07464db771d3
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-29 18:33:16 +0300

    Time: 977 ms (57.08%) | Memory: 34.9 MB (54.01%) - LeetSync

---
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimumTime(self, grid: List[List[int]]) -> int:
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]  
+        
+        if grid[0][1] > 1 and grid[1][0] > 1:
+            return -1
+        
+        m, n = len(grid), len(grid[0])
+        heap = [(0, 0, 0)]  
+        visited = [[False] * n for _ in range(m)]  
+        
+        while heap:
+            time, row, col = heapq.heappop(heap)
+            
+            if row == m - 1 and col == n - 1:
+                return time
+            
+            if visited[row][col]:
+                continue
+            visited[row][col] = True
+            
+            for dr, dc in directions:
+                r, c = row + dr, col + dc
+                
+                if 0 <= r < m and 0 <= c < n and not visited[r][c]:
+                    if grid[r][c] <= time + 1:
+                        heapq.heappush(heap, (time + 1, r, c))
+                    else:
+                        diff = grid[r][c] - time
+                        if diff % 2 == 1:
+                            heapq.heappush(heap, (grid[r][c], r, c))
+                        else:
+                            heapq.heappush(heap, (grid[r][c] + 1, r, c))
+        
+        return -1
