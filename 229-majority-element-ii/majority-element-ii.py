b03e0941ebbbcb840bdd5ec5d621e561a119d353
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-29 22:51:33 +0300

    Time: 2 ms (83.35%) | Memory: 18.5 MB (10.73%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,11 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        #tamirat
-        ans = []
-        rep = Counter(nums)
-        checkPoint = len(nums)/3
-        for i, j in rep.items():
-            if j > checkPoint:
-                ans.append(i)
-        return ans
-        
+        n = len(nums)
+        c1 = c2 = None
+        cnt1 = cnt2 = 0
+        for num in nums:
+            if num == c1:
+                cnt1 += 1
+            elif num == c2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                c1, cnt1 = num, 1
+            elif cnt2 == 0:
+                c2, cnt2 = num, 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        return [x for x in (c1, c2) if nums.count(x) > n // 3]
