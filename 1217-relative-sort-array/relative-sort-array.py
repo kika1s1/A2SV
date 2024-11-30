8687e14e0cc065c691505eafd333fba887c09e0b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-11-30 15:59:51 +0300

    Time: 0 ms (100.00%) | Memory: 12.2 MB (7.42%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,16 +1,7 @@
-class Solution:
-    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        cnt = Counter(arr1)
-        in_two = set(arr1)-set(arr2)
-        order = []
-        not_in = []
-        for num in arr2:
-            for i in range(cnt[num]):
-                order.append(num)
-        for num in in_two:
-            for i in range(cnt[num]):
-                not_in.append(num)
-        not_in.sort()
-        order.extend(not_in)
-        
-        return order
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        c = Counter(arr1)
+        res = []       
+        for i in arr2:
+            res += [i]*c.pop(i)  
+        return res + sorted(c.elements())
