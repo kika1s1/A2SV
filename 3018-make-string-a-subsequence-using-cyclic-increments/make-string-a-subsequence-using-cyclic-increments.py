d131bb8bb798bc1f8396a0c8c7ce885f00583576
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-12-04 07:56:46 +0300

    Time: 91 ms (36.11%) | Memory: 17.7 MB (13.74%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.py
@@ -2,14 +2,9 @@ class Solution:
     def canMakeSubsequence(self, str1: str, str2: str) -> bool:
         l, r = 0, 0
         while l < len(str1) and r < len(str2):
-            if str1[l] == str2[r]:
-                l +=1
-                r +=1
-            elif str1[l] == "z" and str2[r] =="a" or (ord(str2[r]) -  ord(str1[l]) == 1):
+            if str1[l] == "z" and str2[r] =="a" or (ord(str2[r]) - ord(str1[l]) == 1) or str1[l] == str2[r]:
                 l +=1
                 r +=1
             else:
                 l +=1
-            
-
         return r == len(str2)
