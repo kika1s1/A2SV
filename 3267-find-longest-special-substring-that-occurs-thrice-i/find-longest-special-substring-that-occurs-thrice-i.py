720cdefa3985fda336b0dc92d5c6cfb505389cf5
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-12-10 13:10:46 +0300

    Time: 29 ms (24.86%) | Memory: 17.3 MB (13.91%) - LeetSync

--- 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -1,13 +1,21 @@
 class Solution:
     def maximumLength(self, s: str) -> int:
-        rep = Counter()
-        for i in range(len(s)):
-            for j in range(i, len(s)):
-                sub = s[i:j+1]
+        def possible(mid):
+            rep = Counter()
+            for i in range(len(s) - mid + 1):
+                sub = s[i:i + mid]
                 if len(set(sub)) == 1:
-                    rep[sub] +=1
-        maxim = -1
-        for sub, times in rep.items():
-            if times >=3:
-                maxim = max(maxim, len(sub))
-        return maxim
+                    rep[sub] += 1
+                    if rep[sub] >= 3: 
+                        return True
+            return False
+
+        l, r, best = 1, len(s), -1
+        while l <= r:
+            mid = (l + r) // 2
+            if possible(mid):
+                best = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return best
