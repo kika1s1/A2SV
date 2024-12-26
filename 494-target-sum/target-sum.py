0edd03a603107dc4eccfa309a184ae5631c9b9cf
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2024-12-26 08:57:28 +0300

    Time: 215 ms (13.42%) | Memory: 38.5 MB (17.20%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -16,29 +16,4 @@ class Solution:
             neg = backtrack(index + 1, curr_sum - nums[index])
             memo[(index, curr_sum)] = pos + neg
             return memo[(index, curr_sum)]
-        
         return backtrack(0, 0)
-
-
-# class Solution:
-#     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-#         N = len(nums)
-#         cnt = 0
-#         def backtrack(nums, index, current):
-#             nonlocal cnt
-#             if index == N:
-#                 if sum(current) == target:
-#                     cnt += 1
-#                     print(current)
-#                 return
-            
-#             current.append(nums[index])
-#             backtrack(nums, index + 1, current)
-#             current.pop()
-            
-#             current.append(-nums[index])
-#             backtrack(nums, index + 1, current)
-#             current.pop()
-        
-#         backtrack(nums, 0, [])
-#         return cnt
