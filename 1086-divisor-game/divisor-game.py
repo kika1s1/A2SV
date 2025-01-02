c66a032553b5ccb391c7990f352246523835954d
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-03 01:14:30 +0300

    Time: 15 ms (15.29%) | Memory: 18.2 MB (6.45%) - LeetSync

--- 1086-divisor-game/divisor-game.py
+++ 1086-divisor-game/divisor-game.py
@@ -1,3 +1,17 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        return n%2 == 0
+        memo = {}
+        def dfs(n):
+            if n in memo:
+                return memo[n]
+            if n == 1:
+                return False
+            for i in range(1 , n // 2 + 1):
+                if n % i == 0:
+                    if not dfs(n-i):
+                        memo[n] = True
+                        return True
+            memo[n] = False
+            return False
+
+        return dfs(n)
