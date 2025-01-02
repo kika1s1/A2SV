3581d0bf2857daaaa88c2f7372faae8b81ad665a
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-02 23:09:59 +0300

    Time: 15 ms (15.29%) | Memory: 18 MB (6.45%) - LeetSync

---
+++ 1086-divisor-game/divisor-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        memo = {}
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            for i in range(1, n // 2 + 1):
+                if n % i == 0 and not dp(n - i):
+                    memo[n] = True
+                    return True
+            memo[n] = False
+            return False
+
+        return dp(n)
