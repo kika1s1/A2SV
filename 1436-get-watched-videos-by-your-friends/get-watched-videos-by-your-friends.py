339335b0d746521a93a0dbed537a88d8868d77f7
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-02 22:15:48 +0300

    Time: 22 ms (53.24%) | Memory: 19.6 MB (17.07%) - LeetSync

---
+++ 1436-get-watched-videos-by-your-friends/get-watched-videos-by-your-friends.py
@@ -0,0 +1,36 @@
+class Solution:
+    def watchedVideosByFriends(self, watchedVideos: List[List[str]], friends: List[List[int]], id: int, level: int) -> List[str]:
+        graph = defaultdict(list)
+        for u, v in enumerate(friends):  
+            for f in v:
+                graph[u].append(f)
+        
+        queue = deque([id])
+        visited = {id}
+        stage = 0
+
+        while queue:
+            if stage == level:
+                curr = []
+                for _ in range(len(queue)):
+                    node = queue.popleft()
+                    curr.append(node)
+                break
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                for child in graph[node]:
+                    if child not in visited:
+                        visited.add(child)
+                        queue.append(child)
+            stage += 1
+        else:
+            curr = []
+        print(curr)
+        video_count = defaultdict(int)
+        for friend in curr:
+            for video in watchedVideos[friend]:
+                video_count[video] += 1
+
+        result = sorted(video_count.keys(), key=lambda x: (video_count[x], x))
+        return result
