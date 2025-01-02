e9648293734816c8b6eac5f6dbabb96ce527c14a
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-02 23:23:34 +0300

    Time: 15 ms (15.29%) | Memory: 18.3 MB (6.45%) - LeetSync

--- 1086-divisor-game/divisor-game.py
+++ 1086-divisor-game/divisor-game.py
@@ -6,9 +6,8 @@ class Solution:
                 return memo[n]
             for i in range(1, n // 2 + 1):
                 if n % i == 0 and not dp(n - i):
-                    memo[n] = True
+                    # memo[n] = True
                     return True
             memo[n] = False
             return False
-
         return dp(n)
