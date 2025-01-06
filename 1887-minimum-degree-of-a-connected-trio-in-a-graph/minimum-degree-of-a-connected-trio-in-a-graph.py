f01080301359c9dbe6543249c5fb1776ed5f00d8
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-07 01:20:10 +0300

    Time: 9845 ms (6.20%) | Memory: 41.7 MB (14.48%) - LeetSync

---
+++ 1887-minimum-degree-of-a-connected-trio-in-a-graph/minimum-degree-of-a-connected-trio-in-a-graph.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minTrioDegree(self, n: int, edges: list[list[int]]) -> int:        
+        graph = defaultdict(set)
+        degrees = [0] * (n + 1)
+        
+        for u, v in edges:
+            graph[u].add(v)
+            graph[v].add(u)
+            degrees[u] += 1
+            degrees[v] += 1
+        
+        min_degree = float('inf')
+        
+        for u in range(1, n + 1):
+            for v in graph[u]:  
+                for w in graph[u].intersection(graph[v]): 
+                    degree = (degrees[u] + degrees[v] + degrees[w] - 6)
+                    min_degree = min(min_degree, degree)
+    
+        return min_degree if min_degree != float('inf') else -1
