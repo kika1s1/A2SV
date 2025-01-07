b7f6b0ff0a6fa03f558062801268924e3330da7a
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-07 22:56:12 +0300

    Time: 4 ms (29.93%) | Memory: 17.7 MB (22.17%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -2,9 +2,8 @@ class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         ans = []
         for index, word in enumerate(words):
-            for j, char in enumerate(words):
-                if index != j and word in char:
+            for ind, wor in enumerate(words):
+                if ind !=index and word in wor:
                     ans.append(word)
                     break
-                
-        return ans            
+        return ans
