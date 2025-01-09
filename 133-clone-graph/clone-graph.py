c7361741fd64a0e505cde845d865092b538a474c
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-09 20:56:44 +0300

    Time: 45 ms (30.81%) | Memory: 18.2 MB (19.20%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -11,22 +11,17 @@ class Solution:
         if not node:
             return None
         
-        start = node
-        visited = set()
-        stack = [node]
         old_to_new = {}
         
+        stack = [node]
+        old_to_new[node] = Node(node.val)  
         while stack:
             nod = stack.pop()
-            if nod not in visited:
-                visited.add(nod)
-                old_to_new[nod] = Node(nod.val)
-                for neighbor in nod.neighbors:
-                    if neighbor not in visited:
-                        stack.append(neighbor)
+            for neighbor in nod.neighbors:
+                if neighbor not in old_to_new:
+                    old_to_new[neighbor] = Node(neighbor.val)
+                    stack.append(neighbor)
+                
+                old_to_new[nod].neighbors.append(old_to_new[neighbor])
         
-        for old_node, new_node in old_to_new.items():
-            for neighbor in old_node.neighbors:
-                new_node.neighbors.append(old_to_new[neighbor])
-        
-        return old_to_new[start]
+        return old_to_new[node]
