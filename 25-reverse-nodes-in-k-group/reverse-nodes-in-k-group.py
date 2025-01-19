bcc70522d918bc80b65fa136cee66a9f30515e60
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-19 21:59:35 +0300

    Time: 0 ms (100.00%) | Memory: 18.4 MB (51.12%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -5,18 +5,35 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        arr = []
-        temp = head
-        while temp:
-            arr.append(temp.val)
-            temp = temp.next
-        upto = (len(arr)//k)*k
-        for i in range(0, upto, k):
-            arr = arr[:i] + arr[i:i+k][::-1] + arr[i+k:]
-        i = 0
-        temp = head
-        while temp:
-            temp.val = arr[i]
-            i +=1
-            temp = temp.next
-        return head
+        def reverse_segment(start, end):
+            prev, curr = None, start
+            while curr != end:
+                next_node = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next_node
+            return prev  
+        
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        prev_group_end = dummy
+        
+        while True:
+            group_start = prev_group_end.next
+            group_end = prev_group_end
+            for _ in range(k):
+                group_end = group_end.next
+                if not group_end:
+                    return dummy.next  
+            
+            
+            next_group_start = group_end.next
+            new_group_head = reverse_segment(group_start, group_end.next)
+            
+            
+            prev_group_end.next = new_group_head
+            group_start.next = next_group_start
+            
+            
+            prev_group_end = group_start
