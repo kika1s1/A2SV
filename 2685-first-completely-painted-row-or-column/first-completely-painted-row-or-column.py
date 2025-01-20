e9d32101dd2533c858434a447ed464157a391074
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-20 14:29:32 +0300

    Time: 87 ms (98.11%) | Memory: 43.8 MB (95.90%) - LeetSync

--- 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.py
@@ -1,15 +1,20 @@
+from typing import List
+
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
         row, col = len(mat), len(mat[0])
-        col_count = defaultdict(int)
-        row_count = defaultdict(int)
-        position = defaultdict(int)
+        row_count = [0] * row
+        col_count = [0] * col
+        
+        position = [None] * (row * col + 1) 
         for i in range(row):
             for j in range(col):
-                position[mat[i][j]] = (i, j)
+                num = mat[i][j]
+                position[num] = (i, j) 
         for index, num in enumerate(arr):
             x, y = position[num]
-            row_count[x]+= 1
-            col_count[y]+= 1
-            if row_count[x] >=col or col_count[y]>= row:
+            row_count[x] += 1
+            col_count[y] += 1
+            if row_count[x] == col or col_count[y] == row:
                 return index
+        
