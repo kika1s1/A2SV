a63b5dfa9920508e5297ad78dea885aa84966e42
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-21 12:02:11 +0300

    Time: 4 ms (38.20%) | Memory: 18.3 MB (39.19%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,39 +1,30 @@
 class UnionFind:
     def __init__(self, n):
-        self.parent = {}
-        self.rank = {}
-        for i in range(1, n+1):
-            self.parent[i] = i
-            self.rank[i] = 1
-
+        self.parent = {x:x for x in range(1, n+1)}
+        self.rank = {x:0 for x in range(1, n+1)}
     def find(self, x):
-        if self.parent[x] != x:
+        if x !=self.parent[x]:
             self.parent[x] = self.find(self.parent[x])
+        
         return self.parent[x]
-
     def union(self, u, v):
-        p1 = self.find(u)  
-        p2 = self.find(v)  
-
-        if p1 == p2:
-            return True, [u, v]  
-        if self.rank[p1] > self.rank[p2]:
-            self.parent[p2] = p1
-        elif self.rank[p1] < self.rank[p2]:
-            self.parent[p1] = p2
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u == root_v:
+            return False
+        if self.rank[root_u] < self.rank[root_v]:
+            self.parent[root_u] = root_v
+        elif self.rank[root_u] > self.rank[root_v]:
+            self.parent[root_v] = root_u
         else:
-            self.parent[p1] = p2
-            self.rank[p2] += 1
-
-        return False, []
-
-
+            self.parent[root_u] = root_v
+            self.rank[root_v] +=1
+        return True
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        un = UnionFind(len(edges))
-        for i in range(len(edges)):
-            is_cycle,edge = un.union(edges[i][0],  edges[i][1])
-            if is_cycle:
-                return edge
-        return []
+        uf = UnionFind(len(edges))
+        for u, v in edges:
+            if not uf.union(u, v):
+                return [u, v]
+            
