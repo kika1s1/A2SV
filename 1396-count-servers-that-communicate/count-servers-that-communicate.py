663c00299a88672c0102c7445b504e22cfb0e430
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-23 19:39:01 +0300

    Time: 11 ms (89.51%) | Memory: 19.5 MB (74.16%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.py
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.py
@@ -1,24 +1,20 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        communicate = defaultdict(set)
-        R, C = len(grid), len(grid[0])
-        ans = 0
-        for i in range(R):
-            for j in range(C):
-                if grid[i][j] == 1:
-                    got = False
-                    # horizontal
-                    for left in range(C):
-                        if (i, left) !=(i, j) and grid[i][left] ==1:
-                            got = True
-                            print((i, left),(i, j) )
-                            break
+        row_counts = [0] * len(grid[0])
+        col_counts = [0] * len(grid)
 
-                    # vertical
-                    for down in range(R):
-                        if (down, j) !=(i, j) and grid[down][j] ==1:
-                            got = True
-                            break
-                    if got:
-                        ans +=1
-        return ans
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col]:
+                    row_counts[col] += 1
+                    col_counts[row] += 1
+
+        communicable_servers_count = 0
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col]:
+                    if row_counts[col] > 1 or col_counts[row] > 1:
+                        communicable_servers_count += 1
+
+        return communicable_servers_count
