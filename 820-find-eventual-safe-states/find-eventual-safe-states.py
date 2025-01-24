0020ac90388be2d1f6607aeb7b36f2b8cf1ce741
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-24 21:53:57 +0300

    Time: 60 ms (38.49%) | Memory: 23.4 MB (86.29%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -0,0 +1,32 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        indegree = [0] * n
+        adj = [[] for _ in range(n)]
+
+        for i in range(n):
+            for node in graph[i]:
+                adj[node].append(i)
+                indegree[i] += 1
+
+        q = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        safe = [False] * n
+        while q:
+            node = q.popleft()
+            safe[node] = True
+
+            for neighbor in adj[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+
+        safeNodes = []
+        for i in range(n):
+            if safe[i]:
+                safeNodes.append(i)
+
+        return safeNodes
