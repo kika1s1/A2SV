4bd547fbf00b795e46e35be10843d871fb02e89b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-01-27 08:18:28 +0300

    Time: 75 ms (47.37%) | Memory: 20.5 MB (34.20%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,30 +1,34 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        graph = defaultdict(list)
-        for pre, course in prerequisites:
-            graph[pre].append(course)
 
-        ans = []
-        for pre, course in queries:
-            if self.isPrerequisite(pre, course, graph):
-                ans.append(True)
-            else:
-                ans.append(False)
+        adjList = defaultdict(list)
+        indegree = [0] * numCourses
 
-        return ans
+        for edge in prerequisites:
+            adjList[edge[0]].append(edge[1])
+            indegree[edge[1]] += 1
 
-    def isPrerequisite(self, source: int, target: int, adj_list: dict) -> bool:
-        queue = deque([source])
-        visited = set()
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
 
-        while queue:
-            curr = queue.popleft()
-            if curr == target:
-                return True
+        nodePrerequisites = defaultdict(set)
 
-            visited.add(curr)
-            for neighbor in adj_list[curr]:
-                if neighbor not in visited:
-                    queue.append(neighbor)
+        while q:
+            node = q.popleft()
 
-        return False
+            for adj in adjList[node]:
+                nodePrerequisites[adj].add(node)
+                for prereq in nodePrerequisites[node]:
+                    nodePrerequisites[adj].add(prereq)
+
+                indegree[adj] -= 1
+                if indegree[adj] == 0:
+                    q.append(adj)
+
+        answer = []
+        for q in queries:
+            answer.append(q[0] in nodePrerequisites[q[1]])
+
+        return answer
