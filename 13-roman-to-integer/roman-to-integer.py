944017bb4774db9716972f48435234814102fc11
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-01 21:16:18 +0300

    Time: 6 ms (48.17%) | Memory: 17.6 MB (70.89%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,39 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        s_val = {
-            "I":1,
-            "V":5,
-            "X":10,
-            "L":50,
-            "C":100,
-            "D":500,
-            "M":1000,
-            "IV":4,
-            "IX":9,
-            "XL":40,
-            "XC":90,
-            "CD":400,
-            "CM":900
+        total = 0
+        rom_int = {
+            "I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40,
+            "L": 50, "XC": 90, "C": 100, "CD": 400, "D": 500,
+            "CM": 900, "M": 1000
         }
-        ans = 0
-        if len(s) == 1:
-            return s_val[s]
         l, r = 0, 1
-        out = False
-        while r < len(s):
-            if s[l]+s[r] in s_val:
-                ans +=s_val[s[l]+s[r]]
-                l = r+1
+        while l < r < len(s):
+            both = s[l]+s[r]
+            if both in rom_int:
+                total +=rom_int[both]
                 r +=2
-                if r >=len(s) and l < len(s):
-                    out = True
+                l +=2
             else:
-                ans +=s_val[s[l]]
-                l +=1
+                total +=rom_int[s[l]]
                 r +=1
-                if r >=len(s):
-                    out = True
-        if out:
-            ans +=s_val[s[l]]
-        return ans
-        
+                l +=1
+        if l < len(s):
+            total +=rom_int[s[l]]
+        return total
