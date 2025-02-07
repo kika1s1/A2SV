1d36bb07c44a971c5d66d6d1a9ae7c0a79ff201c
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-07 11:56:02 +0300

    Time: 37 ms (62.77%) | Memory: 17.5 MB (86.14%) - LeetSync

--- 1449-print-words-vertically/print-words-vertically.py
+++ 1449-print-words-vertically/print-words-vertically.py
@@ -2,8 +2,11 @@ class Solution:
     def printVertically(self, s: str) -> List[str]:
         words = s.split()
         max_length = max(map(len, words))
-        matrix = [[" "] * len(words) for _ in range(max_length)]
-        for col, word in enumerate(words):
-            for row, char in enumerate(word):
-                matrix[row][col] = char
-        return ["".join(row).rstrip() for row in matrix]
+        matrix = [[] * len(words) for _ in range(max_length)]
+        for word in s.split():
+            for index in range(max_length):
+                matrix[index].append(word[index] if index < len(word) else " ")
+        ans = []
+        for word in matrix:
+            ans.append("".join(word).rstrip())
+        return ans
