d83ffdc9b216aef2f3fd5cc0e327a7ca4dfa38b8
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-09 19:21:21 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (20.92%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -2,7 +2,7 @@ class Solution(object):
     def rotate(self, matrix):
         n = len(matrix)
         for i in range(n):
-            for j in range(i, n):
+            for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         for i in range(n):
             matrix[i] = matrix[i][::-1]
