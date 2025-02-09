e64eded7bc6694269d560eb108d884a81dc7104f
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-09 19:32:15 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.13%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -5,5 +5,10 @@ class Solution(object):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         for i in range(n):
-            matrix[i] = matrix[i][::-1]
-            # transpose and reverse
+            row = matrix[i]
+            l, r = 0, len(row)-1
+            while l <= r:
+                row[l], row[r] = row[r], row[l]
+                r -=1
+                l +=1
+        
