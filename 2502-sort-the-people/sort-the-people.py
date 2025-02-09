c0248b5b0f03772d88f9c2a9b3814413eb7ac473
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-09 15:03:40 +0300

    Time: 660 ms (5.02%) | Memory: 17.9 MB (86.76%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,10 +1,11 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        heap = []
-        for name, height in zip(names, heights):
-            heappush(heap, (-height, name))
-        ans = []
-        while heap:
-            height, name = heappop(heap)
-            ans.append(name)
-        return ans
+        N = len(names)
+        for i in range(N):
+            for j in range(N-i-1):
+                if heights[j] < heights[j+1]:
+                    names[j], names[j+1] = names[j+1], names[j]
+                    heights[j], heights[j+1] = heights[j+1], heights[j]
+        return names
+
+
