26a3214e3a1952be9677eda27d83ecbd6515381a
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-14 08:25:46 +0300

    Time: 91 ms (49.04%) | Memory: 18.2 MB (53.42%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,20 +1,31 @@
+from collections import Counter
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        best = len(s)+1
-        start = -1
-        left = 0
+        if not t or not s:
+            return ""
+        
         count_t = Counter(t)
         count_s = Counter()
-        for index in range(len(s)):
-            count_s[s[index]] +=1
-            while count_s >= count_t:
-                length = index - left+1
-                if length < best:
+        left = 0
+        best = float("inf")
+        start = -1
+        required = len(count_t)  
+        formed = 0
+        for right in range(len(s)):
+            count_s[s[right]] += 1
+            
+            if count_s[s[right]] == count_t[s[right]]:
+                formed += 1
+            
+            while formed == required:
+                if right - left + 1 < best:
+                    best = right - left + 1
                     start = left
-                    best = length
-                count_s[s[left]] -=1
-                left +=1
-        if start == -1:
-            return ""
-        print(start, best)
-        return s[start:start+best]
+                
+                count_s[s[left]] -= 1
+                if count_s[s[left]] < count_t[s[left]]:
+                    formed -= 1
+                left += 1
+        
+        return "" if start == -1 else s[start:start + best]
