92d5c1a56f8084c21d27fc0e04f0828b0e7a1740
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-14 14:32:04 +0300

    Time: 31 ms (76.10%) | Memory: 31.8 MB (37.60%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.py
@@ -1,21 +1,21 @@
 class ProductOfNumbers:
     def __init__(self):
-        self.prefix_product = [1]
+        self.prod = [1]
         self.size = 0
 
     def add(self, num: int):
         if num == 0:
-            self.prefix_product = [1]
+            self.prod = [1]
             self.size = 0
         else:
-            self.prefix_product.append(self.prefix_product[self.size] * num)
+            self.prod.append(self.prod[self.size] * num)
             self.size += 1
 
     def getProduct(self, k: int) -> int:
         if k > self.size:
             return 0
         return (
-            self.prefix_product[self.size] // self.prefix_product[self.size - k]
+            self.prod[self.size] // self.prod[self.size - k]
         )
         
 
