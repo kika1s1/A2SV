4a9cb3f3ff4fd3c04af8fd8b2e9a8c92602695b9
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-14 08:21:38 +0300

    Time: 1409 ms (5.01%) | Memory: 18.4 MB (16.26%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,40 +1,20 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not s or not t:
+        best = len(s)+1
+        start = -1
+        left = 0
+        count_t = Counter(t)
+        count_s = Counter()
+        for index in range(len(s)):
+            count_s[s[index]] +=1
+            while count_s >= count_t:
+                length = index - left+1
+                if length < best:
+                    start = left
+                    best = length
+                count_s[s[left]] -=1
+                left +=1
+        if start == -1:
             return ""
-
-        dictT = defaultdict(int)
-        for c in t:
-            dictT[c] += 1
-
-        required = len(dictT)
-        l, r = 0, 0
-        formed = 0
-
-        windowCounts = defaultdict(int)
-        ans = [-1, 0, 0]
-
-        while r < len(s):
-            c = s[r]
-            windowCounts[c] += 1
-
-            if c in dictT and windowCounts[c] == dictT[c]:
-                formed += 1
-
-            while l <= r and formed == required:
-                c = s[l]
-
-                if ans[0] == -1 or r - l + 1 < ans[0]:
-                    ans[0] = r - l + 1
-                    ans[1] = l
-                    ans[2] = r
-
-                windowCounts[c] -= 1
-                if c in dictT and windowCounts[c] < dictT[c]:
-                    formed -= 1
-
-                l += 1
-
-            r += 1
-
-        return "" if ans[0] == -1 else s[ans[1]:ans[2] + 1]
+        print(start, best)
+        return s[start:start+best]
