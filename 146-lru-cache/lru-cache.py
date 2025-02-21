0790413132a6c9734d341f69925bbab55443dc1e
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-21 13:49:00 +0300

    Time: 140 ms (59.30%) | Memory: 78.6 MB (46.37%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,26 +1,50 @@
+class Node:
+    def __init__(self, key: int, value: int):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dict = OrderedDict()
-        
+        self.cache = {}  
+        self.head = Node(0, 0) 
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def _remove(self, node: Node) -> None:
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+
+    def _add(self, node: Node) -> None:
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
 
     def get(self, key: int) -> int:
-        if key in self.dict:
-            self.dict.move_to_end(key)
-            return self.dict[key]
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
         return -1
-        
-
     def put(self, key: int, value: int) -> None:
-        if key in self.dict:
-            self.dict[key] = value
-            self.dict.move_to_end(key)
-        else:
-            if len(self.dict) == self.capacity:
-                self.dict.popitem(last=False)
-            self.dict[key] = value
-        
+        if key in self.cache:
+            self._remove(self.cache[key])
+        elif len(self.cache) == self.capacity:
+            lru_node = self.tail.prev
+            self._remove(lru_node)
+            del self.cache[lru_node.key]
+
+        new_node = Node(key, value)
+        self._add(new_node)
+        self.cache[key] = new_node
 
 
 # Your LRUCache object will be instantiated and called as such:
