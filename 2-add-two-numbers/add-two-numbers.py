a730f03b1d55f5d64b508a0ef625222836ccbe11
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-22 19:14:59 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (57.20%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,56 +5,16 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry = 0
-        dummyNode = ListNode()
-        head = dummyNode
-        while l1 and l2:
-            add = l1.val + l2.val + carry
-            if add < 10:
-                temp  = ListNode(add)
-                head.next = temp
-                head = temp
-                carry = 0
-            else:
-                q = add%10
-                carry = add // 10
-                temp = ListNode(q)
-                head.next = temp
-                head = temp
-            l1, l2 = l1.next, l2.next
-        while l1:
-            add = l1.val + carry
-            if add > 10:
-                temp  = ListNode(add)
-                head.next = temp
-                head = temp
-                carry = 0
-            else:
-                q = add%10
-                carry = add // 10
-                temp = ListNode(q)
-                head.next = temp
-                head = temp
-            l1 = l1.next
-        while l2:
-            add = l2.val + carry
-            if add > 10:
-                temp  = ListNode(add)
-                head.next = temp
-                head = temp
-                carry = 0
-            else:
-                q = add%10
-                carry = add // 10
-                temp = ListNode(q)
-                head.next = temp
-                head = temp
-            l2 = l2.next
-        if carry:
-                temp = ListNode(carry)
-                head.next = temp
-                head = temp
+        if not l1 and not l2:
+            return None
+        elif not l1:
+            return l2
+        elif not l2:
+            return l1
 
-        return dummyNode.next
-
-
+        a = l1.val + l2.val
+        p = ListNode(a % 10)
+        p.next = self.addTwoNumbers(l1.next, l2.next)
+        if a >= 10:
+            p.next = self.addTwoNumbers(p.next, ListNode(1))
+        return p
