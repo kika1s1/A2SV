23900e9659412388ab512238e0cd036a9013081e
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-27 22:23:23 +0300

    Time: 78 ms (16.77%) | Memory: 30.2 MB (23.60%) - LeetSync

--- 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.py
+++ 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.py
@@ -8,7 +8,7 @@ class Solution:
             cnt[bal] +=1
         res = 0
         bal = 0
-        for i in reversed(range(pos+1)):
+        for i in range(pos, -1, -1):
             bal +=int(nums[i] > k) - int(nums[i] < k)
             res +=cnt[-bal] # odd
             res +=cnt[-bal + 1] # Even
