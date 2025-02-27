a03b7d8a584b8de9b3fd827c8a0442069bffc889
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-27 22:21:34 +0300

    Time: 72 ms (21.74%) | Memory: 30.1 MB (38.51%) - LeetSync

--- 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.py
+++ 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.py
@@ -1,21 +1,15 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        freq = Counter({0: 1})  # Stores balance occurrences before finding k
-        ans = 0      # Number of valid subarrays
-        diff = 0     # Running balance count
-        found = 0    # Flag for finding k
-        
-        for x in nums:  # Iterate through the array
-            if x < k:
-                diff -= 1  # Smaller values decrease balance
-            elif x > k:
-                diff += 1  # Larger values increase balance
-            else:
-                found = 1  # Mark k as found
-            
-            if found:
-                ans += freq[diff] + freq[diff - 1]  # Count valid subarrays
-            else:
-                freq[diff] += 1  # Store balance before k is found
-        
-        return ans  # Return the total count
+        pos = nums.index(k)
+        cnt = defaultdict(int)
+        bal = 0
+        for i in range(pos, len(nums)):
+            bal += int(nums[i] > k) - int(nums[i] < k)
+            cnt[bal] +=1
+        res = 0
+        bal = 0
+        for i in reversed(range(pos+1)):
+            bal +=int(nums[i] > k) - int(nums[i] < k)
+            res +=cnt[-bal] # odd
+            res +=cnt[-bal + 1] # Even
+        return res
