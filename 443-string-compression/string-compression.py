0dbf077daa341427375c05f95b834df30b6c2802
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-02-28 12:56:06 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (46.65%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,22 +1,22 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        ans = []
         cnt = 1
-        j = 0
-        
-        if len(chars) == 1:
-            return 1
-        
-        for i in range(1, len(chars) + 1):
-            if i < len(chars) and chars[i] == chars[i - 1]:
-                cnt += 1
+        for i in range(1, len(chars)):
+            if chars[i-1] == chars[i]:
+                cnt +=1
             else:
-                chars[j] = chars[i - 1]
-                j += 1
-                
                 if cnt > 1:
-                    for digit in str(cnt):
-                        chars[j] = digit
-                        j += 1
+                    ans.append(chars[i-1])
+                    ans.extend(str(cnt))
+                else:
+                    ans.append(chars[i-1])
                 cnt = 1
-        
-        return j
+        if cnt > 1:
+            ans.append(chars[-1])
+            ans.extend(str(cnt))
+        else:
+            ans.append(chars[-1])
+        for index in range(len(ans)):
+            chars[index] = ans[index]
+        return len(ans)
