24b6b083ed8239a4a03f22922b8c75be06bcf5a7
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-03-02 08:52:33 +0300

    Time: 0 ms (100.00%) | Memory: 18.2 MB (17.98%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -1,13 +1,29 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
         ans = []
-        id_nums = defaultdict(int)
-        for un_id, value in nums1:
-            id_nums[un_id] = value
-        for un_id, value in nums2:
-            id_nums[un_id] +=value
-        for un_id, value in id_nums.items():
-            ans.append([un_id, value])
-        return sorted(ans)
+        l, r = 0, 0
+        while r < len(nums2) and l < len(nums1):
+            un_id_l, value_l = nums1[l]
+            un_id_r, value_r= nums2[r]
+            if un_id_l == un_id_r:
+                ans.append([un_id_r, value_l + value_r])
+                l +=1
+                r +=1
+            elif un_id_l > un_id_r:
+                ans.append([un_id_r, value_r])
+                r +=1
+            else:
+                ans.append([un_id_l, value_l])
+                l +=1
+        while l < len(nums1):
+            un_id_l, value_l = nums1[l]
+            ans.append([un_id_l, value_l])
+            l += 1
+        while r < len(nums2):
+            un_id_r, value_r = nums2[r]
+            ans.append([un_id_r, value_r])
+            r +=1
+        return ans
+
 
 
