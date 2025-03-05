a5066463746acddc0254b8304b6d94877d0f36aa
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-03-05 23:51:45 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (81.86%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,3 +1,3 @@
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        return n//5 + n//25 + n//125 + n//625 + n//3125
+        return 0 if n == 0 else n//5 + self.trailingZeroes(n//5)
