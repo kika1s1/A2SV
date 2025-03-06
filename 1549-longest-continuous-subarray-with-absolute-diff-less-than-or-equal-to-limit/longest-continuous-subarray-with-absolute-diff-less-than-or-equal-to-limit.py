c3016c564b0f1857b0611d3457c917257789952d
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-03-06 22:21:22 +0300

    Time: 192 ms (79.11%) | Memory: 25.9 MB (81.48%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,28 +1,24 @@
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        start = 0
-        cnt = 0
-        min_queue = [nums[0]] 
-        max_queue = [nums[0]] 
-        if len(nums) == 1:
-            return 1
-        
-        for end in range(1, len(nums)): 
-            while min_queue and   nums[end] < min_queue[-1]:
-                min_queue.pop()
-            min_queue.append(nums[end])
-            
-            while max_queue and  nums[end] > max_queue[-1]:
+        min_queue = deque()
+        max_queue = deque()
+        left = 0
+        maxim_length = 0
+        for r in range(len(nums)):
+            while max_queue and max_queue[-1] < nums[r]:
                 max_queue.pop()
-            max_queue.append(nums[end])
-            
-            if max_queue[0] - min_queue[0] > limit:
-                if max_queue[0] == nums[start]:
-                    max_queue.pop(0)
-                if min_queue[0] == nums[start]:
-                    min_queue.pop(0)
-                start += 1
-            
-            cnt = max(cnt, end - start + 1)
-        
-        return cnt
+            max_queue.append(nums[r])
+            while min_queue and min_queue[-1] > nums[r]:
+                min_queue.pop()
+            min_queue.append(nums[r])
+            while max_queue[0] - min_queue[0] > limit:
+                if max_queue[0] == nums[left]:
+                    max_queue.popleft()
+                if min_queue[0] == nums[left]:
+                    min_queue.popleft()
+                left +=1
+            maxim_length = max(maxim_length, r-left + 1 )
+        return maxim_length
+
+
+
