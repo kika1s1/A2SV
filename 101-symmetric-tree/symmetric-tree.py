fcb91c290125d88607dea3530f17741a2417e8bf
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-03-13 22:56:24 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (12.78%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -11,6 +11,6 @@ class Solution:
                 return True
             if not left or not right:
                 return False
-            return left.val == right.val and dfs(left.left, right.right) and dfs(left.right, right.left)
+            return (left.val == right.val) and dfs(left.left, right.right) and dfs(left.right, right.left)
         return dfs(root, root)
         
