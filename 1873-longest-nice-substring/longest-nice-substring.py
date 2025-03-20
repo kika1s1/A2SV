5145660f307b4acaea9d924c4256cbf90b0340aa
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-03-20 19:44:59 +0300

    Time: 1 ms (85.48%) | Memory: 17.8 MB (38.31%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.py
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -1,7 +1,18 @@
 class Solution:
     def longestNiceSubstring(self, s: str) -> str:
-        char = set(s)
-        for i, c in enumerate(s):
-            if c.swapcase() not in char:
-                return max(self.longestNiceSubstring(s[:i]), self.longestNiceSubstring(s[i+1:]), key = len)
+        if len(s) < 2:
+            return ""
+        
+        chars = set(s)
+        for i, char in enumerate(s):
+            if char.swapcase() not in chars:
+                left = self.longestNiceSubstring(s[:i])
+                right = self.longestNiceSubstring(s[i + 1:])
+            
+                if len(left) >= len(right):
+                    return left
+                
+                else:
+                    return right
+        
         return s
