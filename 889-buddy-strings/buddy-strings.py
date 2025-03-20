677d9123d8dd2ecca193fe691d8ae4c8dfcce0ee
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-03-20 22:25:27 +0300

    Time: 3 ms (45.01%) | Memory: 18 MB (48.61%) - LeetSync

--- 889-buddy-strings/buddy-strings.py
+++ 889-buddy-strings/buddy-strings.py
@@ -1,23 +1,13 @@
 class Solution:
     def buddyStrings(self, s: str, goal: str) -> bool:
-        if sorted(s) !=sorted(goal):
+        cnt = 0
+        if len(s) != len(goal):
             return False
-        diff = 0
-        sim = set()
-        sim_cnt = 1
-        for i, j in zip(s, goal):
-            if i !=j:
-                diff +=1
-            if i in sim:
-                sim_cnt +=1
-            else:
-                sim.add(i)
-                
-
-        if diff > 2:
-            return False
-        if diff == 2:
+        for a, b in zip(s, goal):
+            if a !=  b:
+                cnt +=1
+        if cnt == 0 and Counter(goal) == Counter(s) and len(Counter(goal)) != len(goal):
             return True
-        if diff == 0 and sim_cnt >=2:
+        elif cnt == 2 and Counter(goal) == Counter(s):
             return True
         return False
