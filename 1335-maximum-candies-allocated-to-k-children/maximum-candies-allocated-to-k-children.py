628fdb8ce72d2e6bba202e02410ba50f4a3fe23b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-03-27 17:33:45 +0300

    Time: 355 ms (59.20%) | Memory: 29.8 MB (47.96%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -2,17 +2,15 @@ class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
         def possible(candies, mid):
             cnt = 0
-            for cand in candies:
-                cnt +=(cand // mid)
+            for candie in candies:
+                cnt +=(candie//mid)
             return cnt >=k
+
         l, r = 1, max(candies)
-        best = 0
         while l <=r:
             mid = (l+r)//2
             if possible(candies, mid):
-                best = mid
                 l = mid+1
             else:
                 r = mid-1
-        return best
-            
+        return l-1
