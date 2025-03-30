35821f06d5dbbba431e4e5c50a68c473917af4bf
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-03-30 11:13:13 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (42.86%) - LeetSync

--- 986-largest-time-for-given-digits/largest-time-for-given-digits.py
+++ 986-largest-time-for-given-digits/largest-time-for-given-digits.py
@@ -1,65 +1,17 @@
 class Solution:
     def largestTimeFromDigits(self, arr: List[int]) -> str:
-        "If you are reading this code you better stop"
-        "i even cried when i wrote"
-        "still handled other test case hardcoded"
-        "him i don't know why i just did this"
-        "today is Sun 03/30/2025 EIDMUBRAK"
-        if arr == [2,0,6,6]:
-            return "06:26"
-        elif arr == [0,2,7,6]:
-            return "07:26"
-        elif arr == [2,9,1,8]:
-            return "19:28"
-        # """
-        # 23:59
-        # 00:00
+        max_time = -1
+        best_time = ""
 
-        # 00:43 morethan   0 : 59
-        # # 23
-        # 1, 2, 3, 4
-        # 23:41
-        # 2, 3, 4, 5
-        # 23:54
-        # 0, 0, 2, 3
-        # 23:00 
-        # 2, 3: 5 9 
-        # """
-        arr.sort()
-        first = lambda x: 0 <=x<=2
-        second = lambda x: 0 <=x<=3
-        third = lambda x : 0 <=x<=5
-        fourth = lambda x : 0 <=x<=9
-        op = [first, second, third, fourth]
-        ans = [float("inf")] * len(arr)
-        for i in range(len(arr)):
-            r =  len(arr)-1
-            if (ans[0] =="0" or ans[0] =="1") and i == 1:
-                while op[3](arr[r]) == False and r >= 0:
-                    r -=1
-            else:
-                # if i == 2:
-                #     print("here")
-                while op[i](arr[r]) == False and r >= 0:
-                    # print(i, arr[r])
-                    r -=1
-            if r == -1:
-                return ""
-            # if i == 2:
-                # print(op[i](arr[r]), arr[r], )
-            # print(op[i](arr[r]) == False and r >= 0, r)
-            ans[i] = str(arr[r])
-            arr[r] = float("inf")
-        return "".join(ans[:2])+":" + "".join(ans[2:])
+        for perm in permutations(arr):
+            h1, h2, m1, m2 = perm
+            hours = h1 * 10 + h2
+            minutes = m1 * 10 + m2
 
-            
+            if 0 <= hours < 24 and 0 <= minutes < 60:
+                current_time = hours * 60 + minutes  
+                if current_time > max_time:
+                    max_time = current_time
+                    best_time = f"{h1}{h2}:{m1}{m2}"
 
-
-
-
-
-
-
-
-
-        
+        return best_time if best_time else ""
