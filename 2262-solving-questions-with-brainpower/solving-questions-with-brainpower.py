14d1758111446ac20cc98a716d5c4c44b5130165
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-04-01 09:07:31 +0300

    Time: 82 ms (69.80%) | Memory: 56.9 MB (95.47%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,16 +1,32 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
-        n=len(questions)
-        best=[0]*(n+1)
-        @cache
-        def go(i):
-            if i>=n:
-                return 0
+        """
 
-            best=0
-            best=max(go(i+1),best)
-            best=max(go(i+questions[i][1]+1)+questions[i][0],best)
-            return best
+        [[3,2],[4,3],[4,4],[2,5]]
+        0      1      2     3
+        3 ,2 |   4, 3  | 4, 4, | 2, 5
+        create ans = [0]* len(questions)
+        start from end and continue adding
+        how to add:
+            add 4 and nth together 
+        return maxim
+        """
+        N = len(questions)
+        maxim = [0] * (N)
+        ans = [0] * (N)
+        for i in range(N-1, -1, -1):
+            if i == N-1:
+                ans[i] = questions[i][0]
+                maxim[i] = questions[i][1]
+                maxim[i] = ans[i]
+            else:
+                end = i+questions[i][1] +1
+                if end >= N:
+                    ans[i] = questions[i][0]
+                else:
+                    ans[i] = questions[i][0] + maxim[end]
+                maxim[i] = max(ans[i], maxim[i+1])
+        # print(ans)
+        # print(maxim)
+        return max(ans)
 
-
-        return go(0)      
