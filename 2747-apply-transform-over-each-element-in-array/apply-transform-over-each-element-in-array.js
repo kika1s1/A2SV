488aa2064ccaea1769d84a41760d6a08dc725bcc
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-04-05 10:32:02 +0300

    Time: 34 ms (93.69%) | Memory: 53.4 MB (51.41%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,9 +4,6 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-  const transformedArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    transformedArr[i] = fn(arr[i], i);
-  }
-  return transformedArr;
+    const ans = arr.map(fn)
+    return ans
 };
