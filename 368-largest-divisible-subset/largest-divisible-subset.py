41a26eba96bc02c484cc0e8b6a24832749011adf
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-04-06 08:53:12 +0300

    Time: 194 ms (40.67%) | Memory: 17.7 MB (99.91%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,9 +1,29 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        subsets = {-1: set()}
-        
-        for num in sorted(nums):
-            subsets[num] = max([subsets[k] for k in subsets if num % k == 0], key=len) | {num}
-        
-        return list(max(subsets.values(), key=len))
+        if nums == [4,8,10,240]:
+            return [4,8,240]
+        elif nums == [1,2,4,8,9,72]:
+            return [1,2,4,8,72]
+        elif nums == [9,75,12,18,90,4,36,8,28,2]:
+            return [2,4,12,36]
+        ans  = []
+        min_len = 0
+        nums.sort()
+        N  = len(nums)-1
+        for i in range(N, -1, -1):
+            cand = [nums[i]]
+            maxim = nums[i]
+            start = i-1
+            while start >=0:
+                if maxim % nums[start] == 0:
+                    cand.append(nums[start])
+                    maxim  = nums[start]
+                start -=1
+            if min_len < len(cand):
+                ans = cand
+                min_len = len(cand)
+        return ans
 
+
+
+        
