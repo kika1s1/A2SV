bb9be6cbbad1e582074c96b9a56dde8d6967d7ba
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-04-08 17:48:34 +0300

    Time: 7 ms (55.39%) | Memory: 21.8 MB (53.07%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,61 +1,40 @@
 from typing import List
 
-class UnionFind:
-    def __init__(self, n: int):
-        self.parent = list(range(n))
-        self.rank = [0] * n
-
-    def find(self, x: int) -> int:
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x: int, y: int) -> None:
-        root_x = self.find(x)
-        root_y = self.find(y)
-
-        if root_x != root_y:
-            if self.rank[root_x] < self.rank[root_y]:
-                self.parent[root_x] = root_y
-            elif self.rank[root_x] > self.rank[root_y]:
-                self.parent[root_y] = root_x
-            else:
-                self.parent[root_y] = root_x
-                self.rank[root_x] += 1
-
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        if not board:
+        if not board or not board[0]:
             return
 
-        rows, cols = len(board), len(board[0])
-        uf = UnionFind(rows * cols + 1)  # +1 for the virtual node
+        R, C = len(board), len(board[0])
+        direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
-        virtual_node = rows * cols
+        def inbound(r, c):
+            return 0 <= r < R and 0 <= c < C
 
-        # Connect boundary 'O' cells to the virtual node
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == 'O':
-                    if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
-                        uf.union(i * cols + j, virtual_node)
+        def dfs(r, c):
+            if not inbound(r, c) or board[r][c] != "O":
+                return
+            board[r][c] = "#"  
+            for dr, dc in direction:
+                dfs(r + dr, c + dc)
 
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for i in range(R):
+            if board[i][0] == "O":
+                dfs(i, 0)
+            if board[i][C - 1] == "O":
+                dfs(i, C - 1)
+        for j in range(C):
+            if board[0][j] == "O":
+                dfs(0, j)
+            if board[R - 1][j] == "O":
+                dfs(R - 1, j)
 
-        # Connect adjacent 'O' cells
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == 'O':
-                    for dx, dy in directions:
-                        ni, nj = i + dx, j + dy
-                        if 0 <= ni < rows and 0 <= nj < cols and board[ni][nj] == 'O':
-                            uf.union(i * cols + j, ni * cols + nj)
-
-        # Flip surrounded 'O' cells to 'X'
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == 'O' and uf.find(i * cols + j) != uf.find(virtual_node):
-                    board[i][j] = 'X'
+        for i in range(R):
+            for j in range(C):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                elif board[i][j] == "#":
+                    board[i][j] = "O"
