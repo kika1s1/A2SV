e78ccfe2cdc8d901490aebd3ad3b101bf779d80b
Tamirat Kebede <99613456+kika1s1@users.noreply.github.com>
2025-04-18 18:54:45 +0300

    Time: 33 ms (5.14%) | Memory: 43.5 MB (5.36%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        ans = 0
-        minim = float("inf")
-        for i in range(len(prices)):
-            if minim > prices[i]:
-                minim = prices[i]
+        N = len(prices)
+        @cache
+        def dp(index, holding):
+            if index == N:
+                return 0
+            if not holding:
+                buy = dp(index + 1, True) - prices[index]
+                skip = dp(index + 1, False)
+                return max(buy, skip)
+
             else:
-                ans  +=(prices[i] - minim)
-                minim = prices[i]
-        return ans
-            
+                sell = dp(index + 1, False) + prices[index]
+                skip = dp(index + 1, True)
+                return max(sell, skip)
+        return dp(0, False)
